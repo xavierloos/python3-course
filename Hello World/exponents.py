09048be25cddae695ccf5776cebe0abbfeb6945e
xavierloos <xavier.loos04@gmail.com>
2021-07-14 15:32:58 +0100

    Exponents in Py

---
+++ Hello World/exponents.py
@@ -0,0 +1,13 @@
+# 1. You really like how the square quilts from last exercise came out, and decide that all quilts that you make will be square from now on.Using the exponent operator, print out how many squares you’ll need for a 6 x6 quilt, a 7 x7 quilt, and an 8 x8 quilt.
+
+# 2. Your 6x6 quilts have taken off so well, 6 people have each requested 6 quilts. Print out how many tiles you would need to make 6 quilts apiece for 6 people.
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 * 6)
+# 7x7 quilt
+print(7 * 7)
+# 8x8 quilt
+print(8 * 8)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print (6 * 6 * 6 * 6)
