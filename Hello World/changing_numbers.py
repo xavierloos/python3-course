edf7105055bf8912050558222b399c35a09a322e
xavierloos <xavier.loos04@gmail.com>
2021-07-14 15:25:22 +0100

    Changin numbers in Py

---
+++ Hello World/changing_numbers.py
@@ -0,0 +1,11 @@
+# 1. You’ve decided to get into quilting! To calculate the number of squares you’ll need for your first quilt let’s create two variables: quilt_width and quilt_length. Let’s make this first quilt 8 squares wide and 12 squares long.
+
+# 2. Print out the number of squares you’ll need to create the quilt!
+
+# 3. It turns out that quilt required a little more material than you have on hand! Let’s only make the quilt 8 squares long. How many squares will you need for this quilt instead?
+
+quilt_width = 8
+quilt_length = 8
+
+#prints number of squares to make a quilt
+print(quilt_width * quilt_length)
