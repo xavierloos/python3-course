46a3f02a906006bbdcc989e155b32dfd2a31e3e7
xavierloos <xavier.loos04@gmail.com>
2021-07-14 15:08:23 +0100

    Variables in Py

---
+++ Hello World/variables.py
@@ -0,0 +1,20 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Salad"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Ramen"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
