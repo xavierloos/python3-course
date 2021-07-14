990212bcca7bb7f355e2072a05c0fd01560ab63e
xavierloos <xavier.loos04@gmail.com>
2021-07-14 17:00:17 +0100

    Multi Line in Py

---
+++ Hello World/multiline_string.py
@@ -0,0 +1,17 @@
+# 1. Assign the string
+
+# Stranger, if you passing meet me and desire to speak to me, why
+#   should you not speak to me?
+# And why should I not speak to you?
+
+# to the variable to_you.
+
+# Assign the string here
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
+"""
+
+
+print(to_you)
