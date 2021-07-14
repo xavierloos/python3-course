cb88c4d1111d21f540821bd4a526aedfe06f7df4
xavierloos <xavier.loos04@gmail.com>
2021-07-14 17:02:07 +0100

    Review

---
+++ Hello World/review.py
@@ -0,0 +1,20 @@
+# 1. Create variables:
+
+# my_age
+# half_my_age
+# greeting
+# name
+# greeting_with_name
+
+# Assign values to each using your knowledge of division and concatenation!
+
+my_age = 28
+half_my_age = 28/2
+greeting = "Buenos Dias, "
+name = "Javi"
+greeting_with_name = greeting + name
+
+print(my_age)
+print(half_my_age)
+
+print(greeting_with_name + "!")
