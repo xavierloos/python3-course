9d86136046d66cc6963e980b3922fb9c74da7616
xavierloos <xavier.loos04@gmail.com>
2021-07-14 15:05:10 +0100

    String in Py

---
+++ Hello World/string.py
@@ -0,0 +1,4 @@
+# Strings
+# Computer programmers refer to blocks of text as strings. In our last exercise, we created the string “Hello world!”. In Python a string is either surrounded by double quotes ("Hello world") or single quotes ('Hello world'). It doesn’t matter which kind you use, just be consistent.
+
+print('Javi')
