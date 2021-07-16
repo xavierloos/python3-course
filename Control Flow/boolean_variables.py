52edc3855433afec3225ec6f8e0e897264054833
xavierloos <xavier.loos04@gmail.com>
2021-07-16 15:31:01 +0100

    Boolean Variables

---
+++ Control Flow/boolean_variables.py
@@ -0,0 +1,21 @@
+# 1. Create a variable named my_baby_bool and set it equal to "true".
+
+# 2. Check the type of my_baby_bool using type(my_baby_bool).
+
+# You’ll have to print it to get the results to display in the terminal.
+
+# 3. It’s not a boolean variable! Boolean values True and False always need to be capitalized and do not have quotation marks.
+
+# Create a variable named my_baby_bool_two and set it equal to True.
+
+# 4. Check the type of my_baby_bool_two and make sure you successfully created a boolean variable.
+
+# You’ll have to print it to get the results to display in the terminal.
+
+my_baby_bool = "true"
+
+print(type(my_baby_bool))
+
+my_baby_bool_two = True
+
+print(type(my_baby_bool_two))
