b38f9c1b27456b4fd8e9a32b70a973f67f5287a2
xavierloos <xavier.loos04@gmail.com>
2021-07-16 15:41:30 +0100

    Relational Operators II

---
+++ Control Flow/relational_operators_II.py
@@ -0,0 +1,23 @@
+# 1. Create an if statement that checks if x and y are equal, print the string below if so:
+
+# "These numbers are the same"
+
+# 2. The nearby college, Calvin Coolidge’s Cool College (or 4C, as the locals call it) requires students to earn 120 credits to graduate. Write an if statement that checks if the student has enough credits to graduate. If they do, return the string:
+
+# "You have enough credits to graduate!"
+
+# Can a student with 120 credits graduate from Calvin Coolidge’s Cool College?
+
+x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+  print("These numbers are the same")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+  print("You have enough credits to graduate!")
