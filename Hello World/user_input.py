7cdd050f9740dee87c5006ccf31b587afbbbaa1d
xavierloos <xavier.loos04@gmail.com>
2021-07-16 15:10:42 +0100

    User input

---
+++ Hello World/user_input.py
@@ -0,0 +1,4 @@
+# Fill in the blanks in the code to complete a statement that asks a user “What is your favorite flightless bird?” and then stores their answer in the variable favorite_flightless_bird.
+
+favorite_flightless_bird = input("What is your favorite flightless bird? ")
+print(favorite_flightless_bird)
