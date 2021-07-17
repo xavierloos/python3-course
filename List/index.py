852b64114dd249a54c47c677895350cbc5f0f9b0
xavierloos <xavier.loos04@gmail.com>
2021-07-17 14:33:38 +0100

    Accessing List Elements

---
+++ List/index.py
@@ -0,0 +1,18 @@
+# 1. Use square brackets ([ and ]) to select the 4th employee from the list employees. Save it to the variable employee_four.
+ 
+# 2. Paste the following code into script.py:
+
+# print(employees[8])
+# What happens? Why?
+ 
+# 3. Selecting an element that does not exist produces an IndexError.
+
+# In the line of code that you pasted, change 8 to an index that exists so that you don’t get an IndexError.
+
+# Run your code again!
+
+employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
+
+employee_four= employees[3]
+
+print(employees[8])
