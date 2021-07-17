4442abb8d2fa4880a5e6b070be7f5965410ef2bd
xavierloos <xavier.loos04@gmail.com>
2021-07-17 12:53:21 +0100

    TypeError in Python

---
+++ Control Flow/Errors in Python/type_error.py
@@ -0,0 +1,30 @@
+# 1. The word got out in the office that you are a pro bug catcher!
+
+# Another peer Alisha pops out of the blue and hands you a program that calculates the area of a triangle, a rectangle, and a circle.
+
+# Run the program to check it out.
+ 
+# 2. Oh no, there’s one TypeError error!
+
+# Can you find it?
+
+# Area Calculator 📏
+
+import math
+
+base = 20
+height = 30
+area = base * height / 2
+
+print("The triangle area is", area)
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is", area )
+    
+radius = 36
+area = math.pi * radius * radius
+    
+print("The circle area is", area)
