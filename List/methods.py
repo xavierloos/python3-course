abada63ed503c106b211fa5c2c3a5bc59a5743d5
xavierloos <xavier.loos04@gmail.com>
2021-07-17 13:18:29 +0100

    Methods in Python

---
+++ List/methods.py
@@ -0,0 +1,12 @@
+# We will be exploring .append() and many other methods in the upcoming exercises but for now take a second to examine and play around with the code for two common list methods.
+
+example_list = [1, 2, 3, 4]
+
+#Using Append
+example_list.append(5)
+# print(example_list)
+
+#Using Remove
+example_list.remove(5)
+# print(example_list)
+
