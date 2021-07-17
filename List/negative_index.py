b3e2982d239c3f3ebf969143ec56b7942cabc7f9
xavierloos <xavier.loos04@gmail.com>
2021-07-17 14:37:51 +0100

    Accessing List Elements: Negative Index

---
+++ List/negative_index.py
@@ -0,0 +1,17 @@
+# 1.Create a called variable last_element.
+
+# Assign the last element in shopping_list to the variable last_element using a negative index.
+
+# 2.Now select the element with index 5 and save it to the variable index5_element.
+
+# 3. Use print to display both index5_element and last_element.
+
+# Note that they are equal to "cereal"!
+
+shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
+
+last_element = shopping_list[-1]
+index5_element = shopping_list[5]
+
+print(last_element)
+print(index5_element)
