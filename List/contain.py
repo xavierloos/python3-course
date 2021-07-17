a6fd41a11e71b4782cb6e792dc0ccd09cefabc1a
xavierloos <xavier.loos04@gmail.com>
2021-07-17 13:06:58 +0100

    List contains

---
+++ List/contain.py
@@ -0,0 +1,12 @@
+# 1. Add any additional string to the end of the list ints_and_strings.
+ 
+# 2. Create a new list called sam_height_and_testscore that contains:
+
+# The string "Sam" (to represent sam’s name)
+# The number 67 (to represent sam’s average grade)
+# The float 85.5 (to represent sam’s score)
+# The boolean True (to represent sam passing the test)
+# Make sure to write the elements in exact order.
+
+ints_and_strings = [1, 2, 3, "four", "five", "Javi"]
+sam_height_and_testscore= ["Sam", 67, 85.5, True]
