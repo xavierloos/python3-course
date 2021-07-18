b21880066526ed3eb4cdc9f5b2c0637c8f6d82bf
xavierloos <xavier.loos04@gmail.com>
2021-07-18 21:10:48 +0100

    The Power of Range!

--- List/Working With List/range.py
+++ List/Working With List/range.py
@@ -10,3 +10,20 @@ print(list(number_list))
 
 zero_to_seven = range(8)
 print(list(zero_to_seven))
+
+# The Power of Range!
+# 1. Modify the range() function that created the range range_five_three such that it:
+# Starts at 5
+# Has a difference of 3 between each item
+# Ends before 15
+
+# 2.Create a range called range_diff_five that:
+# Starts at 0
+# Has a difference of 5 between each item
+# Ends before 40
+
+# Your code below: 
+
+range_five_three = range(5, 15, 3)
+
+range_diff_five = range(0,40,5)
