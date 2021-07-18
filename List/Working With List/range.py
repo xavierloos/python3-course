52ceace8e3161d02ab1ce54f8a54e3d5b667e2b8
xavierloos <xavier.loos04@gmail.com>
2021-07-18 21:07:37 +0100

    Consecutive Lists: Range\

---
+++ List/Working With List/range.py
@@ -0,0 +1,12 @@
+# 1. Modify number_list so that it is a range containing numbers starting at 0 and up to, but not including, 9.
+ 
+# 2. Create a range called zero_to_seven with the numbers 0 through 7.
+# Print the result in list form.
+
+# Your code below: 
+
+number_list = range(9)
+print(list(number_list))
+
+zero_to_seven = range(8)
+print(list(zero_to_seven))
