a05f085414dcabce32e75d60c9e41c5cacd8a5aa
xavierloos <xavier.loos04@gmail.com>
2021-07-18 19:03:57 +0100

    Modifying 2D Lists

---
+++ List/modifying_2D.py
@@ -0,0 +1,35 @@
+# 1. Our school is expanding! We are welcoming a new set of students today from all over the world.
+
+# Using the provided table, create a two-dimensional list called incoming_class to represent the data. Each sublist in incoming_class should contain the name, nationality, and grade for a single student.
+
+# Name	     Nationality	   Grade Level
+# "Kenny"	   "American"	     9
+# "Tanya"	   "Russian"	     9
+# "Madison"	 "Indian"	       7
+
+# Print incoming_class to see our list.
+
+# 2. "Madison" passed an exam to advance a grade. She will be pushed into 8th grade rather than her current 7th in our list.
+
+# Modify the list using double brackets [][] to make the change. Use positive inidices.
+
+# Print incoming_class to see our change.
+ 
+# 3. "Kenny" likes to be called by his nickname "Ken". Modify the list using double brackets [][] to accommodate the change but only using negative indices.
+
+# Print incoming_class to see our change.
+
+#Your code below:
+incoming_class = [
+ [ "Kenny",	"American",	9],
+["Tanya",	"Russian",	9],
+["Madison",	"Indian",	7]
+]
+print(incoming_class )
+
+incoming_class[2][2]=8
+print(incoming_class )
+
+incoming_class[-3][-3]="Ken"
+
+print(incoming_class )
