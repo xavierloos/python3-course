7428aa2015cecb1e575528972710760e2a2a44ae
xavierloos <xavier.loos04@gmail.com>
2021-07-18 21:39:26 +0100

    Sorting Lists II

---
+++ List/Working With List/sorted.py
@@ -0,0 +1,10 @@
+# 1.Use sorted() to order games and create a new list called games_sorted.
+
+# 2.Print both games and games_sorted.How are they different?
+
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+# Your code below:
+games_sorted = sorted(games)
+print(games)
+print(games_sorted)
