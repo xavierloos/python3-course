a2fffe14fb5002e4e3169b93d43bce4e80209c81
xavierloos <xavier.loos04@gmail.com>
2021-07-18 21:31:41 +0100

    Counting in a List

---
+++ List/Working With List/count.py
@@ -0,0 +1,11 @@
+# 1. Mrs. Wilson’s class is voting for class president. She has saved each student’s vote into the list votes.
+# Use .count() to determine how many students voted for "Jake" and save the value to a variable called jake_votes.
+
+# 2.Use print() to examine jake_votes.
+
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie",
+         "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Your code below:
+jake_votes = votes.count("Jake")
+print(jake_votes)
