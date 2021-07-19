0055254708ace998f5b7ffc91902f58e4dab4675
xavierloos <xavier.loos04@gmail.com>
2021-07-19 16:52:17 +0100

    For Loops: Introduction

---
+++ Loops/for.py
@@ -0,0 +1,17 @@
+# 1. Run the code.
+# We should get an IndentationError because the print(game) line is not indented.
+
+# 2.Indent (2 spaces or tab) line 6 so that we don’t get an IndentationError when you run the code.
+# Run the code again!
+
+# 3.Write a for loop that prints each sport in the list sport_games.
+
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+for game in sport_games:
+  print(game)
