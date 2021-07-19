60e7b86d15ccff528191ad2ac7b55db5dc459894
xavierloos <xavier.loos04@gmail.com>
2021-07-19 16:26:41 +0100

    Why Loops?

---
+++ Loops/loops.py
@@ -0,0 +1,13 @@
+# 1.Using 10 print() statements, print out: "This can be so much easier with loops!".
+
+# Write 10 print() statements below!
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
