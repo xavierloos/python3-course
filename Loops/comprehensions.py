ae0c282e7e53a26d1e52a302e10e1d1deceb3743
xavierloos <xavier.loos04@gmail.com>
2021-07-19 19:15:01 +0100

    List Comprehensions: Conditionals

--- Loops/comprehensions.py
+++ Loops/comprehensions.py
@@ -8,4 +8,20 @@ grades = [90, 88, 62, 76, 74, 89, 48, 57]
 
 scaled_grades = [grade + 10 for grade in grades]
 
-print( scaled_grades)
+print(scaled_grades)
+
+# List Comprehensions: Conditionals
+# 1.We have defined a list heights of visitors to a theme park. In order to ride the Topsy Turvy Tumbletron roller coaster, you need to be above 161 centimeters.
+# Using a list comprehension, create a new list called can_ride_coaster that has every element from heights that is greater than 161.
+
+# 2.Print can_ride_coaster.
+
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+
+can_ride_coaster = []
+
+for height in heights:
+  if height > 161:
+    can_ride_coaster.append(height)
+
+print( can_ride_coaster)
