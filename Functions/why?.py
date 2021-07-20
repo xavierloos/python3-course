7c33911b92951c3a944cfcfb2a6bd448b9d43e97
xavierloos <xavier.loos04@gmail.com>
2021-07-20 10:03:45 +0100

    Why Functions?

---
+++ Functions/why?.py
@@ -0,0 +1,25 @@
+# 1.Run the pre-written print() statements to see what they output.
+
+# 2.Write the same set of print statements three more times. Run the code again and see the output.
+
+# 3.Hopefully now you have some perspective about your life without functions!
+
+# First user wants to travel between these two points!
+print("Setting the Empire State Building as the starting point and Time Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
+
+# Second user wants to travel between these two points!
+print("Setting the Empire State Building as the starting point and Time Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
+
+# Third user wants to travel between these two points!
+print("Setting the Empire State Building as the starting point and Time Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
+
+# Fourth user wants to travel between these two points!
+print("Setting the Empire State Building as the starting point and Time Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
