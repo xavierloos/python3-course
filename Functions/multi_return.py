9f93e5262fd8d239d31c382d733f2b615e1bc02d
xavierloos <xavier.loos04@gmail.com>
2021-07-20 17:27:25 +0100

    Update

--- Functions/multi_return.py
+++ Functions/multi_return.py
@@ -16,6 +16,7 @@ def top_tourist_locations_italy():
   return first, second, third
 
 most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
 print(most_popular1) 
 print(most_popular2) 
 print(most_popular3) 
