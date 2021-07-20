49ea551708ceb1ec477d23843fc879c45a56f6ec
xavierloos <xavier.loos04@gmail.com>
2021-07-20 10:19:47 +0100

    Whitespace & Execution Flow

---
+++ Functions/whitespace_and_execution_flow.py
@@ -0,0 +1,26 @@
+# 1.We are going to help our trip planner users figure out if they should travel today based on the weather. Let’s let our users know we can check the weather for them.
+# Write a print() statement that will output Checking the weather for you!.
+
+# 2.We took a look outside and see a bright sunny day. Write a function called weather_check() that will print a message to our users that it’s a great day to travel! The function should output:
+# Looks great outside! Enjoy your trip.
+# Note: Don’t call your function just yet! We will do that in the next step.
+
+# 3.Oh no! It looks like some clouds came in and it started raining. Our users shouldn’t go on a trip in the rain. In our weather_check() function add a second print() statement under the first one which prints a warning message for our travelers! It should print:
+# False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.
+
+# 4.Call the function weather_check().
+
+# 5.Unindent your final print statement in your weather_check() function. Run the program again.
+# What is different?
+
+# Your code below:
+print("Checking the weather for you!")
+
+
+def weather_check():
+    print("Looks great outside! Enjoy your trip.")
+
+
+print("False Alarm, the weather changed! There is a thunderstorm approaching.Cancel your plans and stay inside.")
+
+weather_check()
