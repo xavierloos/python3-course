b100b1dc46a2a59e2b549cb8720d5b280b4744a0
xavierloos <xavier.loos04@gmail.com>
2021-07-20 10:12:24 +0100

    Calling a Function

--- Functions/def.py
+++ Functions/def.py
@@ -14,3 +14,15 @@ def directions_to_timesSq():
     print('Walk 4 mins to 34th St Herald Square train station')
     print('Take the Northbound N, Q, R, or W train 1 stop')
     print('Get off the Times Square 42nd Street stop')
+
+# Calling a Function
+
+# 1.Call the directions_to_timesSq() function.
+# Click Run to see it execute and print out.
+
+# 2.Add an additional print statement to our directions_to_timesSq() function.
+# Have the statement print "Take lots of pictures!"
+# Run your code again and see how your output changes.
+
+
+directions_to_timesSq()
