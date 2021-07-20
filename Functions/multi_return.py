e3b57e691d1338075eb452af60c28eb66c98b2a8
xavierloos <xavier.loos04@gmail.com>
2021-07-20 17:27:39 +0100

    UYpdate

--- Functions/multi_return.py
+++ Functions/multi_return.py
@@ -4,6 +4,7 @@
 # 2.We want to be able to return the three most popular destinations from our function top_tourist_locations_italy().
 # Add a line in the function top_tourist_locations_italy() that will return the values of first, second, third in that exact order.
 
+
 # 3.In order to use our three returned values from top_tourist_locations_italy() we need to assign them to new variables names after we call our function.
 # Set the return of the function top_tourist_locations_italy() to be equal to three new variables called most_popular1, most_popular2, and most_popular3 in that exact order.
 
