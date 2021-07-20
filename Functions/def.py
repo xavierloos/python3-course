fa870bbb217e3ad1ea60b10e1eab691734c4245b
xavierloos <xavier.loos04@gmail.com>
2021-07-20 10:09:17 +0100

    Defining a Function

---
+++ Functions/def.py
@@ -0,0 +1,16 @@
+# 1.Two of the most common NYC attractions include the Empire State Building and Times Square.
+# In travel.py, we’ll write a function that prints the directions via subway from the Empire State Building to Times Square.
+# First, define a function, directions_to_timesSq(). Leave the body of the function empty for now.
+# Note: When we run the code, we will see an error: “IndentationError: expected an indented block”. This will occur when we don’t populate a function with any statements. We will populate it with code in the next step.
+
+# 2.Within the body of the function, use three print() statements to output the following directions:
+# Walk 4 mins to 34th St Herald Square train station
+# Take the Northbound N, Q, R, or W train 1 stop
+# Get off the Times Square 42nd Street stop
+# Remember, if you run your code, you shouldn’t see any output in the terminal at this point. Check out the hint if you want to see how to see the output (we will be doing it in the next section as well!)
+
+# Your code below:
+def directions_to_timesSq():
+    print('Walk 4 mins to 34th St Herald Square train station')
+    print('Take the Northbound N, Q, R, or W train 1 stop')
+    print('Get off the Times Square 42nd Street stop')
