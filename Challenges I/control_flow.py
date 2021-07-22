b2b70393a47435e76da3d35d1dcecaf7bc85100d
xavierloos <xavier.loos04@gmail.com>
2021-07-22 12:32:39 +0100

    3. Always False

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -141,3 +141,22 @@ print(same_name("Colby", "Colby"))
 # should print True
 print(same_name("Tina", "Amber"))
 # should print False
+
+# 3. Always False
+# There are some situations that you normally want to avoid when programming using conditional statements. One example is a contradiction. This occurs when your condition will always be false no matter what value you pass into it. Let’s create an example of a function that contains a contradiction. It will contain a few steps:
+
+# Define the function to accept a single parameter called num
+# Use a combination of <, > and and to create a contradiction in an if statement.
+# If the condition is true, return True, otherwise return False. The trick here is that because we’ve written a contradiction, the condition should never be true, so we should expect to always return False.
+
+# Write your always_false function here:
+def always_false(num):
+  if num >= num or num <=num:
+    return False
+# Uncomment these function calls to test your always_false function:
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
