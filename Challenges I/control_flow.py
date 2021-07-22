58bb59c112e4ce82626972ae6d0fa55c40aab0c1
xavierloos <xavier.loos04@gmail.com>
2021-07-22 12:33:13 +0100

    4. Movie Review

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -160,3 +160,28 @@ print(always_false(-1))
 # should print False
 print(always_false(1))
 # should print False
+
+# 4. Movie Review
+# We want to create a function that will help us rate movies. Our function will split the ratings into different ranges and tell the user how the movie was based on the movie’s rating. Here are the steps needed:
+
+# Define our function to accept a single number called rating
+# If the rating is equal to or less than 5, return "Avoid at all costs!"
+# If the rating was less than 9, return "This one was fun."
+# If neither of the if statement conditions were met, return "Outstanding!"
+
+# Write your movie_review function here:
+def  movie_review(rating):
+  if rating <= 5:
+    return "Avoid at all costs!"
+  elif rating > 5 and rating < 9:
+    return "This one was fun."
+  else:
+    return "Outstanding!"
+
+# Uncomment these function calls to test your movie_review function:
+print(movie_review(9))
+# should print "Outstanding!"
+print(movie_review(4))
+# should print "Avoid at all costs!"
+print(movie_review(6))
+# should print "This one was fun."
