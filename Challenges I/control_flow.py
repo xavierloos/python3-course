333550cfcc07934c46df83b2dccee76cb305a551
xavierloos <xavier.loos04@gmail.com>
2021-07-22 11:30:54 +0100

    5. Not Sum To Ten

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -78,3 +78,25 @@ print(divisible_by_ten(20))
 # should print True
 print(divisible_by_ten(25))
 # should print False
+
+# 5. Not Sum To Ten
+# Finally, we are going to check if the summation of two inputs does not equal ten. Our function will accept two inputs and add them together. If the two numbers added together are not equal to ten, then we will return True, otherwise, we will return False. Here is what we need to do:
+
+# Define the function to accept two parameters, num1 and num2
+# Add the two parameters together
+# Test if the result is not equal to 10
+# If the sum is not equal, return True, otherwise, return False
+
+# Write your not_sum_to_ten function here:
+def not_sum_to_ten(num1, num2):
+  if(num1 + num2) == 10:
+    return False
+  else:
+    return True
+# Uncomment these function calls to test your not_sum_to_ten function:
+print(not_sum_to_ten(9, -1))
+# should print True
+print(not_sum_to_ten(9, 1))
+# should print False
+print(not_sum_to_ten(5,5))
+# should print False
