1c0b933364b4c3af49b309d68980f2ec3ececd32
xavierloos <xavier.loos04@gmail.com>
2021-07-22 12:32:08 +0100

    2. Same Name

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -122,3 +122,22 @@ print(in_range(10, 10, 10))
 # should print True
 print(in_range(5, 10, 20))
 # should print False
+
+# 2. Same Name
+# We need to write a program that checks different names and determines if they are equal. We need to accept two strings and compare them. Here are the steps:
+
+# Define the function to accept two strings, your_name and my_name
+# Test if the two strings are equal
+# Return True if they are equal, otherwise return False
+
+# Write your same_name function here:
+def same_name(your_name, my_name):
+  if your_name == my_name :
+    return True
+  else:
+    return False 
+# Uncomment these function calls to test your same_name function:
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
