3a649c6748356dc17aeb13ad1566d82d2979c3f5
xavierloos <xavier.loos04@gmail.com>
2021-07-22 11:28:52 +0100

    2. Over Budget

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -16,3 +16,24 @@ print(large_power(2, 13))
 # should print True
 print(large_power(2, 12))
 # should print False
+
+# 2. Over Budget
+# Let’s say we are trying to save some money and we are watching our budget. We need to make sure that the result of our spending is less than the total amount we have allocated for each of the categories. Our function will accept a parameter called budget which describes our spending limit. The next four parameters describe what we are spending our money on. We need to sum all of our spendings and compare it to the budget. If we have gone over budget, we will return True. Otherwise we return False. Here are the steps we need:
+
+# Define the function to accept five parameters starting with budget then food_bill, electricity_bill, internet_bill, and rent
+# Calculate the sum of the last four parameters
+# Use if and else statements to test if the budget is less than the sum of the calculated sum from the previous step.
+# If the condition is true, return True otherwise return False
+
+# Write your over_budget function here:
+def over_budget( budget, food_bill, electricity_bill, internet_bill,  rent):
+  sum = food_bill + electricity_bill + internet_bill +  rent
+  if budget < sum:
+    return True
+  else:
+    return False
+# Uncomment these function calls to test your over_budget function:
+print(over_budget(100, 20, 30, 10, 40))
+# should print False
+print(over_budget(80, 20, 30, 10, 30))
+# should print True
