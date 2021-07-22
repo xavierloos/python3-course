cee182d7e2cd2a35cbcae4e822e39ea3dcb66645
xavierloos <xavier.loos04@gmail.com>
2021-07-22 13:21:09 +0100

    3. Larger List

--- Challenges I/list.py
+++ Challenges I/list.py
@@ -24,12 +24,37 @@ print(append_size([23, 42, 108]))
 # Repeat steps 2 and 3 two more times
 # Return the modified list
 
-#Write your function here
-def append_sum(lst):
-  lst.append(lst[-1] + lst[-2])
-  lst.append(lst[-1] + lst[-2])
-  lst.append(lst[-1] + lst[-2])
-  return lst
+# Write your function here
 
-#Uncomment the line below when your function is done
+
+def append_sum(lst):
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    return lst
+
+
+# Uncomment the line below when your function is done
 print(append_sum([1, 1, 2]))
+
+# 3. Larger List
+# Let’s say we are working with two conveyor belts that contain items represented by a numerical ID. If one conveyor belt contains more items than the other, then we need to return the ID of the last item on that belt. In the case where they have the same number of items, return the last item from the first conveyor belt. In our code, we can represent the belts using lists. Here are the steps:
+
+# Define the function to accept two parameters for our two lists of numbers
+# Check if the length of the first list is greater than or equal to the length of the second list
+# If true, then return the last element from the first list. Otherwise, return the last element from the second list
+
+# Write your function here
+
+
+def larger_list(lst1, lst2):
+    if len(lst1) >= len(lst2):
+        return lst1[-1]
+    elif len(lst1) < len(lst2):
+        return lst2[-1]
+    else:
+        return lst1[-1]
+
+
+# Uncomment the line below when your function is done
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
