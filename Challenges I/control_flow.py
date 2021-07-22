60ef3620a38ee9a6cba8266cc81021cff2528a69
xavierloos <xavier.loos04@gmail.com>
2021-07-22 11:29:49 +0100

    3. Twice As Large

--- Challenges I/control_flow.py
+++ Challenges I/control_flow.py
@@ -37,3 +37,23 @@ print(over_budget(100, 20, 30, 10, 40))
 # should print False
 print(over_budget(80, 20, 30, 10, 30))
 # should print True
+
+# 3. Twice As Large
+# In this challenge, we will determine if one number is twice as large as another number. To do this, we will compare the first number with two times the second number. Here are the steps:
+
+# Define our function with two inputs num1 and num2
+# Multiply the second input by 2
+# Use an if statement to compare the result of the last calculation with the first input
+# If num1 is greater then return True otherwise return False
+
+# Write your twice_as_large function here:
+def twice_as_large(num1, num2):
+  if num1 > (num2 * 2):
+    return True
+  else:
+    return False
+# Uncomment these function calls to test your twice_as_large function:
+print(twice_as_large(10, 5))
+# should print False
+print(twice_as_large(11, 5))
+# should print True
