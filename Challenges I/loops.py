c7640192add7b8c9de3106ab7b5efcc34a27a403
xavierloos <xavier.loos04@gmail.com>
2021-07-23 16:31:01 +0100

    4. Same Values

--- Challenges I/loops.py
+++ Challenges I/loops.py
@@ -181,3 +181,26 @@ def max_num(nums):
 
 # Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
+
+# 4. Same Values
+# In this challenge, we need to find the indices in two equally sized lists where the numbers match. We will be iterating through both of them at the same time and comparing the values, if the numbers are equal, then we record the index. These are the steps we need to accomplish this:
+
+# Define our function to accept two lists of numbers
+# Create a new list to store our matching indices
+# Loop through each index to the end of either of our lists
+# Within the loop, check if our first list at the current index is equal to the second list at the current index. If so, append the index where they matched
+# Return our list of indices
+
+# Write your function here
+
+
+def same_values(lst1, lst2):
+    new_list = []
+    for index in range(len(lst1)):
+        if lst1[index] == lst2[index]:
+            new_list.append(index)
+    return new_list
+
+
+# Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
