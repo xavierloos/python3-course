50147eea315c7a3171ddd7efea91e2aec4b61c54
xavierloos <xavier.loos04@gmail.com>
2021-07-23 16:25:52 +0100

    4. Odd Indices

--- Challenges I/loops.py
+++ Challenges I/loops.py
@@ -61,3 +61,28 @@ def delete_starting_evens(lst):
 # Uncomment the lines below when your function is done
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
+
+# 4. Odd Indices
+# This next function will give us the values from a list at every odd index. We will need to accept a list of numbers as an input parameter and loop through the odd indices instead of the elements. Here are the steps needed:
+
+# Define the function header to accept one input which will be our list of numbers
+# Create a new list which will hold our values to return
+# Iterate through every odd index until the end of the list
+# Within the loop, get the element at the current odd index and append it to our new list
+# Return the list of elements which we got from the odd indices.
+
+# Write your function here
+
+
+def odd_indices(lst):
+    new_list = []
+    index = 0
+    while index < len(lst):
+        if index % 2 != 0:
+            new_list.append(lst[index])
+        index += 1
+    return new_list
+
+
+# Uncomment the line below when your function is done
+print(odd_indices([4, 3, 7, 10, 11, -2]))
