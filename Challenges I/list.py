13ecba98be8f5b98605b5d0d85e4bf40df737185
xavierloos <xavier.loos04@gmail.com>
2021-07-23 16:21:17 +0100

    2. Remove Middle

--- Challenges I/list.py
+++ Challenges I/list.py
@@ -117,3 +117,22 @@ def every_three_nums(start):
 
 # Uncomment the line below when your function is done
 print(every_three_nums(91))
+
+# 2. Remove Middle
+# Our next function will remove all elements from a list with an index within a certain range. The function will accept a list, a starting index, and an ending index. All elements with an index between the starting and ending index should be removed from the list. Here are the steps:
+
+# Define the function to accept three parameters: the list, the starting index, and the ending index
+# Get all elements before the starting index
+# Get all elements after the ending index
+# Combine the two partial lists into the result
+# Return the result
+
+# Write your function here
+
+
+def remove_middle(lst, start, end):
+    return lst[:start] + lst[end+1:]
+
+
+# Uncomment the line below when your function is done
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
