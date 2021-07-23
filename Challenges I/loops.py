ea6972e38ecd46414e22361377f069221e6d9c77
xavierloos <xavier.loos04@gmail.com>
2021-07-23 16:27:33 +0100

    1. Larger Sum

--- Challenges I/loops.py
+++ Challenges I/loops.py
@@ -110,3 +110,30 @@ def exponents(bases, powers):
 
 # Uncomment the line below when your function is done
 print(exponents([2, 3, 4], [1, 2, 3]))
+
+# LOOPS ADVANCED
+# 1. Larger Sum
+# We are going to start our advanced challenge problems by calculating which list of two inputs has the larger sum. We will iterate through each of the list and calculate the sums, afterwards we will compare the two and return which one has a greater sum. Here are the steps we need:
+
+# Define the function to accept two input parameters: lst1 and lst2
+# Create two variables to record the two sums
+# Loop through the first list and add up all of the numbers
+# Loop through the second list and add up all of the numbers
+# Compare the first and second sum and return the list with the greater sum
+
+# Write your function here
+
+
+def larger_sum(lst1, lst2):
+    sum1 = 0
+    sum2 = 0
+    for l1 in lst1:
+        sum1 += l1
+    for l2 in lst2:
+        sum2 += l2
+    print(sum1)
+    print(sum2)
+    if sum1 >= sum2:
+        return lst1
+    else:
+        return lst2
