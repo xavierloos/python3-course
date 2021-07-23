80beada5e4ecd9bb341084a1366a35fe3fe1756d
xavierloos <xavier.loos04@gmail.com>
2021-07-23 16:31:35 +0100

    5. Reversed List

--- Challenges I/loops.py
+++ Challenges I/loops.py
@@ -204,3 +204,25 @@ def same_values(lst1, lst2):
 
 # Uncomment the line below when your function is done
 print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# 5. Reversed List
+# For the final challenge, we are going to test two lists to see if the second list is the reverse of the first list. There are a few different ways to approach this, but we are going to try a method that iterates through each of the values in one direction for the first list and compares them against the values starting from the other direction in the second list. Here is what you need to do:
+
+# Define a function that has two input parameters for our lists
+# Loop through every index in one of the lists from beginning to end
+# Within the loop, compare the element in the first list at the current index against the element at the second list’s last index minus the current index. If there was a mismatch, then the lists aren’t reversed and we can return False
+# If the loop ended successfully, then we know the lists are reversed and we can return True.
+
+# Write your function here
+
+
+def reversed_list(lst1, lst2):
+    if lst1 == lst2[::-1]:
+        return True
+    else:
+        return False
+
+
+# Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
