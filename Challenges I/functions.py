36bcd179276dc13c0a3888893cd5cd16f48e0fbb
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:25:03 +0100

    2. Tip

--- Challenges I/functions.py
+++ Challenges I/functions.py
@@ -128,3 +128,23 @@ first_three_multiples(10)
 # should print 10, 20, 30, and return 30
 first_three_multiples(0)
 # should print 0, 0, 0, and return 0
+
+# 2. Tip
+# Let’s say we are going to a restaurant and we decide to leave a tip. We can create a function to easily calculate the amount to tip based on the total cost of the food and a percentage. This function will accept both of those values as inputs and return the amount of money to tip. In order to do this, we will need a few steps:
+
+# Define the function to accept the total cost of the food called total and the percentage to tip as percentage
+# Calculate the tip amount by multiplying the total and percentage and dividing by 100
+# Return the tip amount
+
+# Write your tip function here:
+
+
+def tip(total, percentage):
+    return (total * percentage) / 100
+
+
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
