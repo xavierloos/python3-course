aebab70cf8dbdf62dc31e8b66d0f466ad5db23cb
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:26:00 +0100

    4. Dog Years

--- Challenges I/functions.py
+++ Challenges I/functions.py
@@ -171,3 +171,26 @@ print(introduction("James", "Bond"))
 # should print Bond, James Bond
 print(introduction("Maya", "Angelou"))
 # should print Angelou, Maya Angelou
+
+# 4. Dog Years
+# Let’s create a function which calculates a dog’s age in dog years! This function will accept the name of the dog and the age in human years. It will calculate the age of the dog in dog years and return a string describing the dog’s age. This will require a few steps:
+
+# Define the function called dog_years to accept two parameters: name and age
+# Calculate the age of the dog in dog years
+# Concatenate the string with the dog’s name and age in dog years
+# Return the resulting string
+
+# Write your dog_years function here:
+
+
+def dog_years(name, age):
+    years = age * 7
+    return name + ", you are " + str(years) + " years old in dog years"
+
+
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+print(dog_years("Javi", 28))
