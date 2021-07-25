604e5b591f496976030fb5c7bc0fae68b8b2b632
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:25:31 +0100

    3. Bond, James Bond

--- Challenges I/functions.py
+++ Challenges I/functions.py
@@ -148,3 +148,26 @@ print(tip(10, 25))
 # should print 2.5
 print(tip(0, 100))
 # should print 0.0
+
+# 3. Bond, James Bond
+# It’s time to re-create a famous movie scene through code. Our function is going to concatenate strings together in order to replace James Bond’s name with whatever name we want. The input to our function will be two strings, one for a first name and another for a last name. The function will return a string consisting of the famous phrase but replaced with our inputs. To accomplish this, we need to:
+
+# Define the function to accept two parameters, first_name and last_name
+# Concatenate the string ', ' on to the last_name
+# Concatenate the first_name on to the result of the previous step
+# Concatenate a space on to the result
+# Concatenate the last_name again to the result
+# Return the final string
+
+# Write your introduction function here:
+
+
+def introduction(first_name, last_name):
+    return last_name + ", " + first_name + " " + last_name
+
+
+# Uncomment these function calls to test your introduction function:
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
