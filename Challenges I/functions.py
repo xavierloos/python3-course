c26bb2bd643aaacd0c8d41f7a3b121c21e3bdc49
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:24:34 +0100

    1. First Three Multiples

--- Challenges I/functions.py
+++ Challenges I/functions.py
@@ -102,3 +102,29 @@ print(remainder(15, 14))
 # should print 2
 print(remainder(9, 6))
 # should print 0
+
+# FUNCTIONS ADVANCED
+# 1. First Three Multiples
+# Let’s start by creating a function which both prints and returns values. For this function, we are going to print out the first three multiples of a number and return the third multiple. This means that we are going to print 1, 2, and 3 times the input number and then return the value of 3 times the input number. For this, we are going to need a few steps:
+
+# Define the function header to accept one input parameter called num
+# Print out 1 times num
+# Print out 2 times num
+# Print out 3 times num
+# Return the value of 3 times num
+
+# Write your first_three_multiples function here
+
+
+def first_three_multiples(num):
+    print(num)
+    print(num * 2)
+    print(num * 3)
+    return num * 3
+
+
+# Uncomment these function calls to test your first_three_multiples function:
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
