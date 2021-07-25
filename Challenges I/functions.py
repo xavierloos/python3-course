6769a0ef5c0c7871a776c8820bd42cd159e954c6
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:16:20 +0100

    \1. Tenth Power

---
+++ Challenges I/functions.py
@@ -0,0 +1,18 @@
+# 1. Tenth Power
+# Let’s create some functions which can help us solve math problems! For this first function, we are going to take the tenth power of a number. In order to do this we need to do three things:
+
+# Set up the function header for tenth_power which accepts one parameter
+# Take the tenth power of the input value
+# Return the result
+# Write your tenth_power function here:
+def tenth_power(num):
+    return num ** 10
+
+
+# Uncomment these function calls to test your tenth_power function:
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
