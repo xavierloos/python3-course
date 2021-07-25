ca39a82ef1c61ab53b7ebba9884f0cb6bc5e1e86
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:31:25 +0100

    Strings are lists

---
+++ Strings/list.py
@@ -0,0 +1,7 @@
+# 1.One of the most common things that are represented by strings is names.
+# Save your name as a string to the variable my_name.
+
+# 2.Select the first letter of the variable my_name and save it to first_initial.
+
+my_name = "Javi"
+first_initial = my_name[0]
