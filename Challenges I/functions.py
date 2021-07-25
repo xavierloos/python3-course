29e3b290557b11a7a90fe5e3df25ea7e29784801
xavierloos <xavier.loos04@gmail.com>
2021-07-25 13:16:50 +0100

    2. Square Root

--- Challenges I/functions.py
+++ Challenges I/functions.py
@@ -16,3 +16,23 @@ print(tenth_power(0))
 # 0 to the 10th power is 0
 print(tenth_power(2))
 # 2 to the 10th power is 1024
+
+# 2. Square Root
+# Another useful function for solving math problems is the square root function. We can create this using similar steps from the last problem. The code will look very similar. We need to:
+
+# Set up the function header for square_root which accepts one parameter
+# Take the square root of the input value
+# Return the result
+
+# Write your square_root function here:
+
+
+def square_root(num):
+    return num ** 0.5
+
+
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
