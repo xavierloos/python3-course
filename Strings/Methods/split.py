0ca98b9788bfcca692854eb9082f7fb8f05e8481
xavierloos <xavier.loos04@gmail.com>
2021-07-26 13:45:13 +0100

    Splitting Strings III

--- Strings/Methods/split.py
+++ Strings/Methods/split.py
@@ -21,3 +21,27 @@ for name in author_names:
     author_last_names.append(name.split()[-1])
 
 print(author_last_names)
+
+# 1.The organization has sent you over the full text for William Carlos Williams poem Spring Storm. They want you to break the poem up into its individual lines.
+# Create a list called spring_storm_lines that contains a string for each line of Spring Storm.
+
+spring_storm_text = \
+    """The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split("\n")
