e794e26a1c1a4dd55a70f358a5f1c59a3fbb53ad
xavierloos <xavier.loos04@gmail.com>
2021-07-26 13:42:24 +0100

    Splitting Strings II

--- Strings/Methods/split.py
+++ Strings/Methods/split.py
@@ -4,3 +4,20 @@
 line_one = "The sky has given over"
 
 line_one_words = line_one.split()
+
+
+# 1.Your boss at the Poetry organization sent over a bunch of author names that he wants you to prepare for importing into the database. Annoyingly, he sent them over as a long string with the names separated by commas.
+# Using .split() and the provided string, create a list called author_names containing each individual author name as it’s own string.
+
+# 2.Great work, but now it turns out they didn’t want poet’s first names (why didn’t they just say that the first time!?)
+# Create another list called author_last_names that only contains the last names of the poets in the provided string.
+
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+
+author_last_names = []
+for name in author_names:
+    author_last_names.append(name.split()[-1])
+
+print(author_last_names)
