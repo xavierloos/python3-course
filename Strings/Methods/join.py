04c69effbd71980e7915fb8b5aa630141c9e39a4
xavierloos <xavier.loos04@gmail.com>
2021-07-26 15:15:41 +0100

    Joining Strings

---
+++ Strings/Methods/join.py
@@ -0,0 +1,9 @@
+# 1.You’ve been provided with a list of words from the first line of Jean Toomer’s poem Reapers.
+# Use .join() to combine these words into a sentence and save that sentence as the string reapers_line_one.
+
+reapers_line_one_words = ["Black", "reapers", "with",
+                          "the", "sound", "of", "steel", "on", "stones"]
+
+reapers_line_one = " ".join(reapers_line_one_words)
+
+print(reapers_line_one)
