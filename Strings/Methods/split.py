a78cf6444fcb3bfcaa6920dadc4b5446fb607215
xavierloos <xavier.loos04@gmail.com>
2021-07-26 13:35:20 +0100

    Splitting Strings

---
+++ Strings/Methods/split.py
@@ -0,0 +1,6 @@
+# 1.In the code editor is a string of the first line of the poem Spring Storm by William Carlos Williams.
+# Use .split() to create a list called line_one_words that contains each word in this line of poetry.
+
+line_one = "The sky has given over"
+
+line_one_words = line_one.split()
