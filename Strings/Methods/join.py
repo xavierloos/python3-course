7bf62d36505378f0b446eb5004ed43f68472af49
xavierloos <xavier.loos04@gmail.com>
2021-07-26 15:20:35 +0100

    Joining Strings II

--- Strings/Methods/join.py
+++ Strings/Methods/join.py
@@ -7,3 +7,12 @@ reapers_line_one_words = ["Black", "reapers", "with",
 reapers_line_one = " ".join(reapers_line_one_words)
 
 print(reapers_line_one)
+
+# 1.You’ve been given a list, winter_trees_lines, that contains all the lines to William Carlos Williams poem, Winter Trees. You’ve been asked to join together the strings in the list together into a single string that can be used to display the full poem. Name this string winter_trees_full.
+# Print your result to the terminal. Make sure that each line of the poem appears on a new line in your string.
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among',
+                      'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = "\n".join(winter_trees_lines)
+print(winter_trees_full)
