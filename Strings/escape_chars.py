fc2ee037e1cbbd80165c49c7369a39efa746a4be
xavierloos <xavier.loos04@gmail.com>
2021-07-26 12:11:45 +0100

    Escape Characters

---
+++ Strings/escape_chars.py
@@ -0,0 +1,5 @@
+# 1.When Rob Daily (remember him? From the last exercise?) set up his account he set his password to be
+# theycallme"crazy"91
+# His password was causing some errors in the system because of the " marks. Rewrite his password using escape characters and save it to the variable password.
+
+password = "theycallme\"crazy\"91"
