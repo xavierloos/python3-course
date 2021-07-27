48a7cb0a630d4d65bf3bc8315b725e4e5c8d4a55
xavierloos <xavier.loos04@gmail.com>
2021-07-27 16:06:43 +0100

    .find()

---
+++ Strings/Methods/find.py
@@ -0,0 +1,6 @@
+# 1.In the code editor is the first line of Gabriela Mistral’s poem God Wills It.
+# At what index place does the word “disown” appear? Save that index place to the variable disown_placement.
+
+god_wills_it_line_one = "The very earth will disown you"
+
+disown_placement = god_wills_it_line_one.find("disown")
