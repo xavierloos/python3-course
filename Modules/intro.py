aeee2b3c2d22515aeb94c8eb721f8acd27c5b421
xavierloos <xavier.loos04@gmail.com>
2021-08-02 13:55:28 +0100

    Intro to Modules

---
+++ Modules/intro.py
@@ -0,0 +1,12 @@
+# 1.In script.py import the datetime type from the datetime library.
+
+# 2.Create a variable current_time and set it equal to datetime.now().
+
+# 3.Print out current_time.
+
+# Import datetime from datetime below:
+from datetime import datetime
+
+current_time = datetime.now()
+
+print(current_time)
