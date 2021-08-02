93b2d3fe73fd3f153a249411b88058290be36841
xavierloos <xavier.loos04@gmail.com>
2021-08-02 15:37:40 +0100

    Introduction to Dictionary

---
+++ Dictionary/intro.py
@@ -0,0 +1,12 @@
+# 1.Suppose we have a dictionary of temperature sensors in the house and what temperatures they read. We’ve just added a sensor to the "pantry", and it reads 22 degrees.
+# Add this pair to the dictionary on line 1.
+
+# 2.Remove the # in front of the definition of the dictionary num_cameras, which represents the number of cameras in each area around the house.
+# If you run this code, you’ll get an error:
+# SyntaxError: invalid syntax
+# Try to find and fix the syntax error to make this code run.
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20,  "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
