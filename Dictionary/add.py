716b41bed8e6e5f3624020dc71ee2f96da350da9
xavierloos <xavier.loos04@gmail.com>
2021-08-02 16:12:18 +0100

    Add A Key

---
+++ Dictionary/add.py
@@ -0,0 +1,19 @@
+# 1. Create an empty dictionary called animals_in_zoo.
+
+# 2. Walking around the zoo, you see 8 zebras. Add "zebras" to animals_in_zoo as a key with a value of 8.
+
+# 3. The primate house was bananas! Add "monkeys" to animals_in_zoo as a key with a value of 12.
+
+# 4. As you leave the zoo, you are saddened that you did not see any dinosaurs. Add "dinosaurs" to animals_in_zoo as a key with a value of 0.
+
+# 5. Print animals_in_zoo.
+
+animals_in_zoo = {}
+
+animals_in_zoo["zebras"] = 8
+
+animals_in_zoo["monkeys"] = 12
+
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
