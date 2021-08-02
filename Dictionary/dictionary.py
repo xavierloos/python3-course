256773fde700c3c9fe03d98808287e5d1a8cc38c
xavierloos <xavier.loos04@gmail.com>
2021-08-02 15:39:57 +0100

    Make a Dictionary

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,10 @@
+# Create a dictionary called translations that maps the following words in English to their definitions in Sindarin (the language of the elves):
+
+# English	Sindarin
+# mountain	orod
+# bread	bass
+# friend	mellon
+# horse	roch
+
+translations = {"mountain": "orod", "bread": "bass",
+                "friend":	"mellon", "horse":	"roch"}
