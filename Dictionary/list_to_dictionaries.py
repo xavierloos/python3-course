adc835561be328100195d2c6ec980c9f2f01578d
xavierloos <xavier.loos04@gmail.com>
2021-08-02 16:21:28 +0100

    List Comprehensions to Dictionaries

---
+++ Dictionary/list_to_dictionaries.py
@@ -0,0 +1,10 @@
+# 1. You have two lists, representing some drinks sold at a coffee shop and the milligrams of caffeine in each. First, create a variable called zipped_drinks that is a zipped list of pairs between the drinks list and the caffeine list.
+
+# 2. Create a dictionary called drinks_to_caffeine by using a list comprehension that goes through the zipped_drinks list and turns each pair into a key: value item.
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine)
+
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
