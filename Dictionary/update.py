e717d0eb3dbfdc2633ad981ff8cecf8ff4a2ab82
xavierloos <xavier.loos04@gmail.com>
2021-08-02 16:14:33 +0100

    Add Multiple Keys

---
+++ Dictionary/update.py
@@ -0,0 +1,11 @@
+# 1. In one line of code, add two new users to the user_ids dictionary:
+# theLooper, with an id of 138475
+# stringQueen, with an id of 85739
+
+# 2. Print user_ids.
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+
+print(user_ids)
