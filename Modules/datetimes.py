176fc11dfeb89448ce364f7ad49907a40f2b4812
xavierloos <xavier.loos04@gmail.com>
2021-08-02 14:52:38 +0100

    Datetime

---
+++ Modules/datetimes.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+bithday = datetime(1992, 10, 4, 5, 15, 23)
+
+bithday.year
+
+bithday.day
+
+bithday.month
+
+# TERMINAL
+# % python3
+# >>> from datetime import datetime
+# >>> bithday = datetime(1992, 10, 4, 5, 15, 23)
+# >>> bithday.year
+# 1992
+# >>> bithday.month
+# 10
+# >>> datetime.now()
+# datetime.datetime(2021, 8, 2, 14, 42, 20, 677498)
+# >>> datetime.now() - datetime(1992,10,4)
+# datetime.timedelta(days=10529, seconds=53139, microseconds=722545)
+# >>> parsed_date = datetime.strptime("Jan 01, 2019", "%b %d, %Y")
+# >>> parsed_date.year
+# 2019
+# >>> date_string = datetime.strftime(datetime.now(), "%b %d, %Y")
+# >>> date_string
+# 'Aug 02, 2021'
