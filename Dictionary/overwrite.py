0cc84c6fe885b0eea17e4b78295472007ef785c6
xavierloos <xavier.loos04@gmail.com>
2021-08-02 16:16:53 +0100

    Overwrite Values

---
+++ Dictionary/overwrite.py
@@ -0,0 +1,10 @@
+# 1. Add the key "Supporting Actress" and set the value to "Viola Davis".
+
+# 2. Without changing the definition of the dictionary oscar_winners, change the value associated with the key "Best Picture" to "Moonlight".
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck",
+                 "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+oscar_winners["Supporting Actress"] = "Viola Davis"
+
+oscar_winners["Best Picture"] = "Moonlight"
