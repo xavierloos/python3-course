bd345a376cc997056c12019726dea0b116fdd545
xavierloos <xavier.loos04@gmail.com>
2021-08-03 21:39:29 +0100

    Update

--- Strings/concatenate.py
+++ Strings/concatenate.py
@@ -6,7 +6,10 @@
 first_name = "Julie"
 last_name = "Blevins"
 
-def account_generator(first_name,last_name):
-  account = first_name[:3] + last_name[:3]
-  return account
-new_account = account_generator(first_name,last_name)
+
+def account_generator(first_name, last_name):
+    account = first_name[:3] + last_name[:3]
+    return account
+
+
+new_account = account_generator(first_name, last_name)
