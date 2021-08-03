5bdcf36c68a997bf95db1e9d3228ac1dae15412f
xavierloos <xavier.loos04@gmail.com>
2021-08-03 21:39:12 +0100

    Update

--- Dictionary/intro.py
+++ Dictionary/intro.py
@@ -6,7 +6,8 @@
 # SyntaxError: invalid syntax
 # Try to find and fix the syntax error to make this code run.
 
-sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20,  "pantry": 22}
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+
 num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
 
 print(sensors)
