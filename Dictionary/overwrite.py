2eb85c974af08219897e50688c54fb25b93e873a
xavierloos <xavier.loos04@gmail.com>
2021-08-03 21:37:44 +0100

    Update

--- Dictionary/overwrite.py
+++ Dictionary/overwrite.py
@@ -6,4 +6,5 @@ oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck",
                  "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
 
 oscar_winners["Supporting Actress"] = "Viola Davis"
+
 oscar_winners["Best Picture"] = "Moonlight"
