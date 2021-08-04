398f5b0d7c2ce8d69bbc5e182ffc1fa4a269728b
xavierloos <xavier.loos04@gmail.com>
2021-08-04 15:26:03 +0100

    SCRABBLE: Step 3 - 6

--- Dictionary/scrabble.py
+++ Dictionary/scrabble.py
@@ -12,3 +12,23 @@ letter_to_points = {key: value for key, value in zip(letters, points)}
 # 2. Our letters list did not take into account blank tiles. Add an element to the letter_to_points dictionary that has a key of " " and a point value of 0.
 
 letter_to_points[" "] = 0
+
+# Score a Word
+# 3.We want to create a function that will take in a word and return how many points that word is worth.
+# Define a function called score_word that takes in a parameter word.
+
+# 4. Inside score_word, create a variable called point_total and set it to 0.
+
+# 5.After defining point_total, create a for loop that goes through the letters in word and adds the point value of each letter to point_total.
+# You should get the point value from the letter_to_points dictionary. If the letter you are checking for is not in letter_to_points, add 0 to the point_total.
+
+# 6. After the for loop is finished, return point_total.
+
+
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        for k, v in letter_to_points.items():
+            if letter == k:
+                point_total += v
+    return point_total
