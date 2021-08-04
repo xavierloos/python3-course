96efd86808427fbb1b1522757d46a9caf1360bb2
xavierloos <xavier.loos04@gmail.com>
2021-08-04 15:23:09 +0100

    SCRABBLE: Step 1

---
+++ Dictionary/scrabble.py
@@ -0,0 +1,10 @@
+# Build your Point Dictionary
+# 1.We have provided you with two lists, letters and points. We would like to combine these two into a dictionary that would map a letter to its point value.
+# Using a list comprehension and zip, create a dictionary called letter_to_points that has the elements of letters as the keys and the elements of points as the values.
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
+          3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letter_to_points = {key: value for key, value in zip(letters, points)}
