f611d206ed99b66332654d0a805add8ab3b2f75b
xavierloos <xavier.loos04@gmail.com>
2021-08-04 13:56:17 +0100

    Get a Key

---
+++ Dictionary/keys.py
@@ -0,0 +1,10 @@
+# 1.We have provided a dictionary that maps the elements of astrology to the zodiac signs. Print out the list of zodiac signs associated with the "earth" element.
+
+# 2.Print out the list of the "fire" signs.
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": [
+    "Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"]}
+
+print(zodiac_elements["earth"])
+
+print(zodiac_elements["fire"])
