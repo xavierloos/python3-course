1d53546f34420a210e2b06a9b36c521e82537f8f
xavierloos <xavier.loos04@gmail.com>
2021-08-04 14:00:48 +0100

    Get an Invalid Key

--- Dictionary/invalid_key.py
+++ Dictionary/invalid_key.py
@@ -6,3 +6,14 @@
 
 children = {"von Trapp": ["Johannes", "Rosmarie",
                           "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+# 1.Run the code. It should throw a KeyError! "energy" does not exist as one of the elements.
+
+# 2.Add the key "energy" to the zodiac_elements. It should map to a value of "Not a Zodiac element". Run the code. Did this resolve the KeyError?
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": [
+    "Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"], "energy": "Not a Zodiac element"}
+
+# print(zodiac_elements["energy2"])
+
+print(zodiac_elements["energy"])
