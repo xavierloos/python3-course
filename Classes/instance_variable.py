10aaee78be80af816215cece31ea111aa829b1ad
xavierloos <xavier.loos04@gmail.com>
2021-08-06 16:05:13 +0100

    Instance Variables

---
+++ Classes/instance_variable.py
@@ -0,0 +1,13 @@
+# 1.In script.py we have defined a Store class. Create two objects from this store class, named alternative_rocks and isabelles_ices.
+
+# 2.Give them both instance attributes called store_name. Set alternative_rocks‘s store_name to "Alternative Rocks". Set isabelles_ices‘s store_name to "Isabelle's Ices".
+
+class Store:
+    pass
+
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
