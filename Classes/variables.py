9882772e3785cfa8fcf9b8df9ec8167315a9ff8c
xavierloos <xavier.loos04@gmail.com>
2021-08-06 14:30:19 +0100

    Class Variables

---
+++ Classes/variables.py
@@ -0,0 +1,5 @@
+# 1.You are digitizing grades for Jan van Eyck High School and Conservatory. At Jan van High, as the students call it, 65 is the minimum passing grade.
+# Create a Grade class with a class attribute minimum_passing equal to 65.
+
+class Grade:
+    minimum_passing = 65
