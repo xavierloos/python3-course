886717fecbfe8b24d0c7f3132db56f76c404d17d
xavierloos <xavier.loos04@gmail.com>
2021-08-06 14:24:25 +0100

    Object-Oriented Programming

---
+++ Classes/OOP.py
@@ -0,0 +1,13 @@
+# 1.In script.py we see facade_1 from last exercise. Try calling type() on facade_1 and saving it to the variable facade_1_type.
+
+# 2.Print out facade_1_type.
+
+class Facade:
+    pass
+
+
+facade_1 = Facade()
+
+facade_1_type = type(facade_1)
+
+print(facade_1_type)
