4fa19e89a3cd9d97eab9e90cb4bab98da4dc876f
xavierloos <xavier.loos04@gmail.com>
2021-08-06 14:01:18 +0100

    Types

---
+++ Classes/type.py
@@ -0,0 +1,19 @@
+# 1.Call type() on the integer 5 and print the results.
+
+# 2.Define a dictionary my_dict.
+
+# 3.Print out the type() of my_dict.
+
+# 4.Define a list called my_list.
+
+# 5.Print out the type() of my_list.
+
+print(type(5))
+
+my_dict = {}
+
+print(type(my_dict))
+
+my_list = []
+
+print(type(my_list))
