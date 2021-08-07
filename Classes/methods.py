90639893b954b3aff66ba5e5050bfcc960c60a98
xavierloos <xavier.loos04@gmail.com>
2021-08-07 13:37:28 +0100

    Update

--- Classes/methods.py
+++ Classes/methods.py
@@ -8,11 +8,15 @@ class Rules:
         return "Point bristles towards the basin while washing your brushes."
 
 # ANOTHER EXAMPLE
+
+
 class Dog:
     dog_time_dilation = 7
+
     def time_explanation(self):
         print("Dogs experience {} years for every 1 human year.".format(
             self.dog_time_dilation))
+
 
 pipi_pitbull = Dog()
 
