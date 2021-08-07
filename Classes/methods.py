5bec793814d347c5817fa3b74b0a9983151c0b06
xavierloos <xavier.loos04@gmail.com>
2021-08-07 13:36:56 +0100

    Update

--- Classes/methods.py
+++ Classes/methods.py
@@ -7,7 +7,6 @@ class Rules:
     def washing_brushes(self):
         return "Point bristles towards the basin while washing your brushes."
 
-
 # ANOTHER EXAMPLE
 class Dog:
     dog_time_dilation = 7
