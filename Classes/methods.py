f253db8d55d8135d3efa835eb75f4970198b09e0
xavierloos <xavier.loos04@gmail.com>
2021-08-07 13:37:06 +0100

    Update

--- Classes/methods.py
+++ Classes/methods.py
@@ -10,7 +10,6 @@ class Rules:
 # ANOTHER EXAMPLE
 class Dog:
     dog_time_dilation = 7
-
     def time_explanation(self):
         print("Dogs experience {} years for every 1 human year.".format(
             self.dog_time_dilation))
