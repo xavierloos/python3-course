0b0d05ec175b66019b5a8d02313e6bff32149e05
xavierloos <xavier.loos04@gmail.com>
2021-08-08 21:15:31 +0100

    Update

--- Files/CSV/CSV.py
+++ Files/CSV/CSV.py
@@ -4,4 +4,4 @@
 # 2.Print out the contents of logger.csv by calling .read() on the file. Notice that it is parsed as a string.
 
 with open("logger.csv") as log_csv_file:
-  print(log_csv_file.read())
+    print(log_csv_file.read())
