8a46863442485c41a69acd30463b9c4727f6a1ee
xavierloos <xavier.loos04@gmail.com>
2021-08-09 15:32:58 +0100

    Review: Step 2

--- Classes/review.py
+++ Classes/review.py
@@ -1,3 +1,6 @@
 # 1.Define a class Student this will be our data model at Jan van Eyck High School and Conservatory.
+# 2.Add a constructor for Student. Have the constructor take in two parameters: a name and a year. Save those two as attributes .name and .year.
 class Student():
-    pass
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
