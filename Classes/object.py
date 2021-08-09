b6c1b7c9616271485de2e80970651b46a0c506cb
xavierloos <xavier.loos04@gmail.com>
2021-08-09 15:29:41 +0100

    Everything is an Object: Step 3

--- Classes/object.py
+++ Classes/object.py
@@ -3,5 +3,12 @@ dir(5)
 
 # 2.Define a function called this_function_is_an_object. It can take any parameters and return anything you’d like.
 
+
 def this_function_is_an_object():
     return "Hello"
+
+# 3.Print out the result of calling dir() on this_function_is_an_object.
+# Functions are objects too!
+
+
+print(dir(this_function_is_an_object))
