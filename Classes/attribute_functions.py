fcefa1be46be12e5bb5181717afe3fb72438aa5c
xavierloos <xavier.loos04@gmail.com>
2021-08-09 15:23:32 +0100

    Attribute Functions: Step 3

--- Classes/attribute_functions.py
+++ Classes/attribute_functions.py
@@ -11,3 +11,11 @@ for element in can_we_count_it:
         print(str(type(element)) + " has the count attribute!")
     else:
         print(str(type(element)) + " does not have the count attribute :(")
+
+# 3.Let’s go over the terminal output of the past two instructions. You should see the following output in your terminal right now:
+# <class 'dict'> does not have the count attribute :(
+# <class 'str'> has the count attribute!
+# <class 'int'> does not have the count attribute :(
+# <class 'list'> has the count attribute!
+# This is because dictionaries and integers both do not have a count attribute, while strings and lists do. In this exercise, we have iterated through can_we_count_it and used hasattr() to determine which elements have a count attribute. We never actually used the count method, but you can read more about it here if you are curious about what it is.
+# Click run to move onto the next exercise!
