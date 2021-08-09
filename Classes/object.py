13f31d961c43c68d1a6b7f7b3458c3b881dd52e9
xavierloos <xavier.loos04@gmail.com>
2021-08-09 15:29:05 +0100

    Everything is an Object: Step 2

--- Classes/object.py
+++ Classes/object.py
@@ -3,6 +3,5 @@ dir(5)
 
 # 2.Define a function called this_function_is_an_object. It can take any parameters and return anything you’d like.
 
-
 def this_function_is_an_object():
     return "Hello"
