0487e9c2cc66d1d57d70ddce2963ac6d7b33eb43
xavierloos <xavier.loos04@gmail.com>
2021-08-10 17:08:36 +0100

    Step 3

--- Classes/Basta_Fazoolin.py
+++ Classes/Basta_Fazoolin.py
@@ -79,3 +79,7 @@ class Menu:
         self.items = items
         self.start_time = start_time
         self.end_time = end_time
+
+
+brunch_menu = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50,
+               'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
