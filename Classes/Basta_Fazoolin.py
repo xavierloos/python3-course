afad746f860f014f81c41bcebba095bacb830153
xavierloos <xavier.loos04@gmail.com>
2021-08-10 17:13:55 +0100

    Step 9

--- Classes/Basta_Fazoolin.py
+++ Classes/Basta_Fazoolin.py
@@ -83,6 +83,13 @@ class Menu:
     def __repr__(self):
         return "{name} menu available from {start_time} to {end_time}".format(name=self.name, start_time=self.start_time, end_time=self.end_time)
 
+    def calculate_bill(self, purchased_items):
+        bill = 0
+        for item in purchased_items:
+            if item in self.items:
+                bill += self.items[item]
+        return bill
+
 
 brunch_menu = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50,
                'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
