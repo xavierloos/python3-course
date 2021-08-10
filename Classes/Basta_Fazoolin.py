74493636d6120ad30bc41e33ba1303e27170f5f6
xavierloos <xavier.loos04@gmail.com>
2021-08-10 17:08:56 +0100

    Step 4

--- Classes/Basta_Fazoolin.py
+++ Classes/Basta_Fazoolin.py
@@ -83,3 +83,6 @@ class Menu:
 
 brunch_menu = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50,
                'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
+
+early_bird_menu = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00,
+                   'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}
