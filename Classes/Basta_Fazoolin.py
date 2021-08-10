955617caf705d0a4c44bbdb059142a6c7d7250a4
xavierloos <xavier.loos04@gmail.com>
2021-08-10 17:11:10 +0100

    Step 7

--- Classes/Basta_Fazoolin.py
+++ Classes/Basta_Fazoolin.py
@@ -80,6 +80,9 @@ class Menu:
         self.start_time = start_time
         self.end_time = end_time
 
+    def __repr__(self):
+        return "{name} menu available from {start_time} to {end_time}".format(name=self.name, start_time=self.start_time, end_time=self.end_time)
+
 
 brunch_menu = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50,
                'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
