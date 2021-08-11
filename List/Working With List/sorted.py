4e66953538414ae2f136e1c222817f3b9f8596ff
xavierloos <xavier.loos04@gmail.com>
2021-08-11 21:48:44 +0100

    Challenge (Advanced) # 2. Every Other Letter

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -77,3 +77,30 @@ print(check_for_name("My name is jamie", "Jamie"))
 # should print True
 print(check_for_name("My name is Samantha", "Jamie"))
 # should print False
+
+
+# 2. Every Other Letter
+# For this next function, we are going to create a function that extract every other letter from a string and returns the resulting string. There are a few different ways you can solve this problem Here are the steps needed for one of the ways:
+# Define the function to accept one parameter for the string
+# Create a new empty string to hold every other letter from the input string
+# Loop through the input string while incrementing by two every time
+# Inside the loop, append the character at the current location to the new string we initialized earlier
+# Return the new string
+
+def every_other_letter(word):
+    word_str = []
+    indx = 0
+    # for w in word:
+    while indx < len(word):
+        word_str.append(word[indx])
+        indx += 2
+    return "".join(word_str)
+
+
+# Uncomment these function calls to test your function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print
