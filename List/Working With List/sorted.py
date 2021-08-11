c0fdabeb9415cbc25eaf5cec446e052032df370a
xavierloos <xavier.loos04@gmail.com>
2021-08-11 21:36:24 +0100

    Challenge (Advanced) # 1. Check Name

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -54,3 +54,26 @@ print(x_length_words("i like apples", 2))
 # should print False
 print(x_length_words("he likes apples", 2))
 # should print True
+
+# Strings (Advanced)
+# 1. Check Name
+# You are creating an app that allows users to interact and share their coding project ideas online. The first step is to provide your name in the application and a greeting for other people to see which contains your name. Let’s create a function that is able to check if a user’s name is located within their greeting. We need a function that accepts two parameters, a string for our sentence and a string for a name. The function should return True if the name exists within the string (ignoring any differences in capitalization). Here is what we need to do:
+# Define the function to accept two parameters, one string for the sentence and one string for the name
+# Convert all of the strings to the same case so we don’t have to worry about differences in capitalization
+# Check if the name is within the sentence. If so, then return True. Otherwise, return False
+
+
+def check_for_name(sentence, name):
+    if name.lower() in sentence.lower():
+        return True
+    else:
+        return False
+
+
+# Uncomment these function calls to test your  function:
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
