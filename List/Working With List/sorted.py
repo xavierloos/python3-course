4e56bcfa747e2078c8ab60369a4d09d00705cb7d
xavierloos <xavier.loos04@gmail.com>
2021-08-11 21:19:08 +0100

    Challenge: # 4. Substring Between

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -8,3 +8,26 @@ games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
 games_sorted = sorted(games)
 print(games)
 print(games_sorted)
+
+# 4. Substring Between
+# Here is a challenging problem. We need a function that is able to extract a portion of a string between two characters. The function will take three parameters where the first parameter is the string we are going to extract the substring from, the second input is the starting character of our substring and the third character is the ending character of our substring. Here are the steps we can use:
+# Define the function to accept three parameters, one string and two characters
+# find the starting index of our substring using the second input parameter
+# find the ending index of our substring using the third input parameter
+# If neither of the indices are -1, return the portion of the first input parameter string between the starting and ending indices (not including the starting and ending index)
+# If either of the indices are -1, that means the start or end of the substring didn’t exist in our string. Return the original string in this case.
+
+
+def substring_between_letters(word, start, end):
+    start_ind = word.find(start)
+    end_ind = word.find(end)
+    if start_ind > -1 and end_ind > -1:
+        return(word[start_ind+1:end_ind])
+    return word
+
+
+# Uncomment these function calls to test your function:
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
