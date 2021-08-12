b86cf020d849f7d49adf3c1e1723e1c327657f61
xavierloos <xavier.loos04@gmail.com>
2021-08-12 14:13:04 +0100

    Challenge (Advanced) # 4. Make Spoonerism

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -128,3 +128,28 @@ print(reverse_string("Hello world!"))
 # should print !dlrow olleH
 print(reverse_string(""))
 # should print
+
+# 4. Make Spoonerism
+# A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism is made when someone says “Belly Jeans” instead of “Jelly Beans”. We are going to make a function that is similar, but instead of using the first syllable, we are going to switch the first character of two words. Here is what we need to do:
+# Define the function to accept two parameters for our two words
+# Get the first character of the first word and the first character of the second word
+# Get the remaining characters of the first word and the remaining characters of the second word.
+# Append the first character of the second word to the remaining character of the first word
+# Append a space character
+# Append the first character of the first word to the remaining characters of the second word.
+# Return the result
+
+
+def make_spoonerism(word1, word2):
+    word_1 = word2[0] + word1[1:]
+    word_2 = word1[0] + word2[1:]
+    return word_1 + " " + word_2
+
+
+# Uncomment these function calls to test your function:
+print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+print(make_spoonerism("a", "b"))
+# should print b a
