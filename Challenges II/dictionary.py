dd15775ba69c5b6bbaeed9b537989c9616aa21f3
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:52:29 +0100

    Challenge (Advanced) # 4. Count First Letter

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -181,3 +181,30 @@ print(unique_values({0: 3, 1: 1, 4: 1, 5: 3}))
 # should print 2
 print(unique_values({0: 3, 1: 3, 4: 3, 5: 3}))
 # should print 1
+
+# 4. Count First Letter
+# This function accepts a dictionary where the keys are last names and the values are lists of first names of people who have that last name. We need to calculate the number of people who have the same first letter in their last name. Here are the steps we need:
+# Define the function to accept one parameter for our dictionary
+# Create a new empty dictionary called letters
+# Loop through every key in our names dictionary
+# Inside the loop, get the first letter of the last name we are looking at. If the first letter is not in our letter dictionary, add it as a key with a value of 0. Then, increment that key by the number of people that have that last name
+# After the loop, return the letters dictionary
+
+
+def count_first_letter(names):
+    letters = {}
+    for key in names:
+        first_letter = key[0]
+        if first_letter not in letters:
+            letters[first_letter] = 0
+        letters[first_letter] += len(names[key])
+    return letters
+
+
+# Uncomment these function calls to test your  function:
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow": [
+      "Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow": [
+      "Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
