933a088a54847327ccc9d0e72b5045fcf2a88534
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:40:34 +0100

    Challenge # 4. Values That Are Keys

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -61,3 +61,26 @@ print(add_ten({1: 5, 2: 2, 3: 3}))
 # should print {1:15, 2:12, 3:13}
 print(add_ten({10: 1, 100: 2, 1000: 3}))
 # should print {10:11, 100:12, 1000:13}
+
+# 4. Values That Are Keys
+# We are making a program that will create a family tree. Using a dictionary, we want to return a list of all the children who are also parents of other children. Using dictionaries we can consider those people to be values which are also keys in our dictionary of family data. Here is what we need to do:
+# Define the function to accept one parameter for our dictionary
+# Create an empty list to hold the values we find
+# Loop through every value in the dictionary
+# Inside the loop, test if the current value is a key in the dictionary. If it is then append it to the list of values we found
+# After the loop, return the list of values which are also keys
+
+
+def values_that_are_keys(my_dictionary):
+    value_keys = []
+    for value in my_dictionary.values():
+        if value in my_dictionary:
+            value_keys.append(value)
+    return value_keys
+
+
+# Uncomment these function calls to test your  function:
+print(values_that_are_keys({1: 100, 2: 1, 3: 4, 4: 10}))
+# should print [1, 4]
+print(values_that_are_keys({"a": "apple", "b": "a", "c": 100}))
+# should print ["a"]
