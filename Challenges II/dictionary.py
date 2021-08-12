fe00e7f6c701f02aed5533e6f0513cdca2fd1747
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:17:29 +0100

    Challenge # 3. Add Ten

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -41,3 +41,23 @@ print(sum_even_keys({1: 5, 2: 2, 3: 3}))
 # should print 2
 print(sum_even_keys({10: 1, 100: 2, 1000: 3}))
 # should print 6
+
+# 3. Add Ten
+# Let’s loop through the keys again, but this time let’s modify the values within the dictionary. Our function should add 10 to every value in the dictionary and return the modified dictionary. Here is what we need to do:
+# Define the function to accept one parameter for our dictionary
+# Loop through every key in the dictionary
+# Retrieve the value using the key and add 10 to it. Make sure to re-save the new value to the original key.
+# After the loop, return the modified dictionary
+
+
+def add_ten(my_dictionary):
+    for key in my_dictionary.keys():
+        my_dictionary[key] += 10
+    return my_dictionary
+
+
+# Uncomment these function calls to test your  function:
+print(add_ten({1: 5, 2: 2, 3: 3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10: 1, 100: 2, 1000: 3}))
+# should print {10:11, 100:12, 1000:13}
