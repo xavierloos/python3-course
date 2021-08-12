2a0f92f08dee2b3b7ab0f2975eeb2e5610960f4e
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:46:21 +0100

    Challenge (Advanced)  # 1. Word Length Dict

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -110,3 +110,27 @@ print(max_key({1: 100, 2: 1, 3: 4, 4: 10}))
 # should print 1
 print(max_key({"a": 100, "b": 10, "c": 1000}))
 # should print "c"
+
+# Dictionaries (Advanced)
+
+# 1. Word Length Dict
+# Let’s start by writing a function that creates a new dictionary based on a list of strings. The keys of our dictionary will be every string in our list of strings, while the values will be the length of each of the words in the string list. Here are the steps:
+# Define the function to accept one parameter for our list of strings
+# Create a new empty dictionary
+# Loop through every string in the list of strings
+# Inside the loop, add the string as a key and the length of the string as the value to the dictionary
+# After the loop, return the new dictionary
+
+
+def word_length_dictionary(words):
+    word_lengths = {}
+    for word in words:
+        word_lengths[word] = len(word)
+    return word_lengths
+
+
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
