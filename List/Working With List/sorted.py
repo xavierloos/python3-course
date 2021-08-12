7b3f026c3dd2a4be2de1880c9a96b71f58987b1b
xavierloos <xavier.loos04@gmail.com>
2021-08-12 14:02:31 +0100

    Challenge (Advanced) # 3. Reverse

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -104,3 +104,27 @@ print(every_other_letter("Hello world!"))
 # should print Hlowrd
 print(every_other_letter(""))
 # should print
+
+# 3. Reverse
+# This one is similar to the last challenge. Instead of selecting every other letter, we want to reverse the entire string. This can be performed in a similar manner, but we will need to modify the range we are using. Here is what we need to do:
+# Define the function to accept one parameter for the string
+# Create a new empty string to hold the reversed string
+# Loop through the input string by starting at the end, and working towards the beginning
+# Inside the loop, append the character at the current location to the new string we initialized earlier
+# Return the result
+
+
+def reverse_string(word):
+    reverse = ""
+    for i in range(len(word)-1, -1, -1):
+        reverse += word[i]
+    return reverse
+
+
+# Uncomment these function calls to test your  function:
+print(reverse_string("Codecademy"))
+# should print ymedacedoC
+print(reverse_string("Hello world!"))
+# should print !dlrow olleH
+print(reverse_string(""))
+# should print
