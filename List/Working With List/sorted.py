d274288f09e886388b226c5aaf88f46752bd274f
xavierloos <xavier.loos04@gmail.com>
2021-08-12 14:27:22 +0100

    Challenge (Advanced) # 5. Add Exclamation

--- List/Working With List/sorted.py
+++ List/Working With List/sorted.py
@@ -153,3 +153,23 @@ print(make_spoonerism("Hello", "world!"))
 # should print wello Horld!
 print(make_spoonerism("a", "b"))
 # should print b a
+
+# 5. Add Exclamation
+# Let’s say we are writing a program that displays a large message on a blimp and we need to fill in any missing space if a short phrase is provided. Our function will accept a string and if the size is less than 20, it will fill in the remaining space with exclamation marks until the size reaches 20. If the provided string already has a length greater than 20, then we will simply return the original string. Here are the steps:
+# Define the function to accept one parameter for our string
+# Loop while the length of our input string is less than 20
+# Inside the loop, append an exclamation mark
+# Once done, return the result
+
+
+def add_exclamation(word):
+    while(len(word) < 20):
+        word += "!"
+    return word
+
+
+# Uncomment these function calls to test your function:
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
