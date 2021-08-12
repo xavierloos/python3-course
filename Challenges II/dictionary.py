d5306ca0102d49b9853478610faf62974eb3736a
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:12:09 +0100

    Challenge # 2. Even Keys

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -18,3 +18,26 @@ print(sum_values({"milk": 5, "eggs": 2, "flour": 3}))
 # should print 10
 print(sum_values({10: 1, 100: 2, 1000: 3}))
 # should print 6
+
+# 2. Even Keys
+# Next, we are going to do something similar, but we are going to use the keys in order to retrieve the values. Additionally, we are going to only look at every even key within the dictionary. Here are the steps:
+# Define the function to accept one parameter for our dictionary
+# Create a variable to keep track of our sum
+# Loop through every key in the dictionary
+# Inside the loop, if the key is even, add the value from the even key
+# After the loop, return the sum
+
+
+def sum_even_keys(my_dictionary):
+    sum_key = 0
+    for key in my_dictionary.keys():
+        if key % 2 == 0:
+            sum_key += my_dictionary[key]
+    return sum_key
+
+
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1: 5, 2: 2, 3: 3}))
+# should print 2
+print(sum_even_keys({10: 1, 100: 2, 1000: 3}))
+# should print 6
