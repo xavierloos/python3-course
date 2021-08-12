da310a096cc34aba676080d286bcf230f968f557
xavierloos <xavier.loos04@gmail.com>
2021-08-12 15:47:59 +0100

    Challenge (Advanced) # 2. Frequency Count

--- Challenges II/dictionary.py
+++ Challenges II/dictionary.py
@@ -134,3 +134,27 @@ print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}
 print(word_length_dictionary(["a", ""]))
 # should print {"a": 1, "": 0}
+
+# 2. Frequency Count
+# This next function is similar, but instead of counting the length of each string in the list of strings, we will be counting the frequency of each string. This function will also accept a list of strings as input and return a new dictionary. Here is what we need to do:
+# Define the function to accept one parameter for our list of strings
+# Create a new empty dictionary
+# Loop through every string in the list of strings
+# Inside the loop, if the string is not already in our dictionary, store the string as a key with a value of 0 in our dictionary. Then, increment the value by 1.
+# After the loop, return the new dictionary
+
+
+def frequency_dictionary(words):
+    freqs = {}
+    for word in words:
+        if word not in freqs:
+            freqs[word] = 0
+        freqs[word] += 1
+    return freqs
+
+
+# Uncomment these function calls to test your  function:
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0, 0, 0, 0, 0]))
+# should print {0:5}
