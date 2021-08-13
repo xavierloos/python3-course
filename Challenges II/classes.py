4939af4227a40ef69984376cf6bfeff42f42a6b4
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:23:28 +0100

    Challenge (Advanced) # 2. Using The Superclass

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -233,3 +233,69 @@ robot_3 = WalkBot(20, 90, 15, 10)
 print(robot_2.id)
 print(robot_3.step_length)
 print(robot_1.speed)
+
+# 2. Using The Superclass
+# There was an issue discovered when testing the WalkBot prototypes. In some cases, the robots were incorrectly detecting their own legs as obstacles. To overcome this, we need to modify our adjust_sensor method to reset obstacle_found to False and step_length to 5 while also using the original logic from the superclass. Here are the steps:
+# Override the adjust_sensor method in the WalkBot class by re-defining it in that class.
+# Call the superclass version of the method within adjust_sensor
+# Add a line of code to set obstacle_found to False
+# Add a line of code to set step_length to 5
+
+
+class Robot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, speed=0, direction=180, sensor_range=10):
+        self.speed = speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        self.obstacle_found = False
+        Robot.robot_count += 1
+        self.id = Robot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+    def avoid_obstacles(self):
+        if self.obstacle_found:
+            self.direction = (self.direction + 180) % 360
+            self.obstacle_found = False
+
+
+class DriveBot(Robot):
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        super().__init__(motor_speed, direction, sensor_range)
+
+
+class WalkBot(Robot):
+
+    def __init__(self, steps_per_minute=0, direction=180, sensor_range=10, step_length=5):
+        super().__init__(steps_per_minute, direction, sensor_range)
+        self.step_length = step_length
+
+    # Override the adjust_sensor method here!
+    def adjust_sensor(self, new_sensor_range):
+        super().adjust_sensor(new_sensor_range)
+        self.obstacle_found = False
+        self.step_length = 5
+
+
+robot_walk = WalkBot(60, 90, 10, 15)
+robot_walk.obstacle_found = True
+print(robot_walk.sensor_range)
+print(robot_walk.obstacle_found)
+print(robot_walk.step_length)
+# Call the overridden adjust_sensor method here!
+robot_walk.adjust_sensor(25)
+
+print(robot_walk.sensor_range)
+print(robot_walk.obstacle_found)
+print(robot_walk.step_length)
