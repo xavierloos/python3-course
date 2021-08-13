b56c70bc4be30037c57106e5a3f38bb99a9c535b
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:24:42 +0100

    Challenge (Advanced) # 3. Conditional Superclass Logic

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -299,3 +299,86 @@ robot_walk.adjust_sensor(25)
 print(robot_walk.sensor_range)
 print(robot_walk.obstacle_found)
 print(robot_walk.step_length)
+
+# 3. Conditional Superclass Logic
+# Since the bipedal robot is a bit less stable, the obstacle avoidance must be more delicate. Because of this, we want to call the superclass method for avoiding obstacles within the bipedal robot class if the steps per minute are less than or equal to 60, otherwise the WalkBot should only rotate 90 degrees clockwise and set obstacle_found to False. In either case, we need to half the robot’s speed and step_length in order to slow it down during the turn. Here are the steps we need to do:
+# Within the WalkBot class, override the method called avoid_obstacles by re-defining it in the class.
+# If an obstacle was found
+# If the speed if less than or equal to 60
+# Call the superclass version of the method
+# Otherwise add 90 to the WalkBot‘s direction. If the new direction is greater than 360, it should loop back around to be between 0 and 360. For example, if the new direction is 370, it should really be 10. (hint: use modulo to do this!)
+# You should also set obstacle_found to False is the speed was over 60.
+# Regardless of whether an obstacle was found, half the speed and the step_length of the robot
+
+
+class Robot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, speed=0, direction=180, sensor_range=10):
+        self.speed = speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        self.obstacle_found = False
+        Robot.robot_count += 1
+        self.id = Robot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+    def avoid_obstacles(self):
+        if self.obstacle_found:
+            self.direction = (self.direction + 180) % 360
+            self.obstacle_found = False
+
+
+class DriveBot(Robot):
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        super().__init__(motor_speed, direction, sensor_range)
+
+
+class WalkBot(Robot):
+
+    def __init__(self, steps_per_minute=0, direction=180, sensor_range=10, step_length=5):
+        super().__init__(steps_per_minute, direction, sensor_range)
+        self.step_length = step_length
+
+    def adjust_sensor(self, new_sensor_range):
+        super().adjust_sensor(new_sensor_range)
+        self.obstacle_found = False
+        self.step_length = 5
+
+    # Override the avoid_obstacles method here!
+    def avoid_obstacles(self):
+        if(self.obstacle_found):
+            if(self.speed <= 60):
+                super().avoid_obstacles()
+            else:
+                self.direction = (self.direction + 90) % 360
+                self.obstacle_found = False
+            self.speed /= 2
+            self.step_length /= 2
+
+
+robot_1 = WalkBot(150, 0, 10, 10)
+robot_1.obstacle_found = True
+robot_1.avoid_obstacles()
+
+print(robot_1.direction)
+print(robot_1.speed)
+print(robot_1.step_length)
+
+robot_2 = WalkBot(60, 0, 20, 40)
+robot_2.obstacle_found = True
+robot_2.avoid_obstacles()
+
+print(robot_2.direction)
+print(robot_2.speed)
+print(robot_2.step_length)
