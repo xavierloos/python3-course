3c4e40d8507b22c314eaa7fc32417b6e3b09f36b
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:26:44 +0100

    Challenge (Advanced) # 4. Overriding Dunder Methods

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -382,3 +382,110 @@ robot_2.avoid_obstacles()
 print(robot_2.direction)
 print(robot_2.speed)
 print(robot_2.step_length)
+
+# 4. Overriding Dunder Methods
+# Let’s add an easy way to increase and decrease our speed as well as some other attributes depending on the type of robot. For the Robot class, we want to increase and decrease the speed using the + and - operators. For the DriveBot class, we want to adjust the speed and sensor_range with those operators. Lastly, for the WalkBot class, we want to adjust the speed and step_length with those operators. We can override the dunder methods which represent those operations and call the superclass version of those dunder methods. Here are the steps:
+# In the Robot class, override the + operator to add to the speed of the robot
+# In the Robot class, override the - operator to subtract from the speed of the robot
+# In the DriveBot class, override the + operator to call the superclass version of the operator as well as add to the sensor_range of the robot
+# In the DriveBot class, override the - operator to call the superclass version of the operator as well as subtract from the sensor_range of the robot
+# In the WalkBot class, override the + operator to call the superclass version of the operator as well as add half of the value on the right side of the operator to the step_length of the robot
+# In the WalkBot class, override the - operator to call the superclass version of the operator as well as subtract half of the value on the right side of the operator from the step_length of the robot
+
+
+class Robot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, speed=0, direction=180, sensor_range=10):
+        self.speed = speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        self.obstacle_found = False
+        Robot.robot_count += 1
+        self.id = Robot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+    def avoid_obstacles(self):
+        if self.obstacle_found:
+            self.direction = (self.direction + 180) % 360
+            self.obstacle_found = False
+
+  # Override the + and - operations here!
+
+    def __add__(self, value):
+        self.speed += value
+
+    def __sub__(self, value):
+        self.speed -= value
+
+
+class DriveBot(Robot):
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        super().__init__(motor_speed, direction, sensor_range)
+
+    # Override the + and - operations here while using those dunder methods from the superclass!
+
+    def __add__(self, value):
+        super().__add__(value)
+        self.sensor_range += value
+
+    def __sub__(self, value):
+        super().__sub__(value)
+        self.sensor_range -= value
+
+
+class WalkBot(Robot):
+
+    def __init__(self, steps_per_minute=0, direction=180, sensor_range=10, step_length=5):
+        super().__init__(steps_per_minute, direction, sensor_range)
+        self.step_length = step_length
+
+    def adjust_sensor(self, new_sensor_range):
+        super().adjust_sensor(new_sensor_range)
+        self.obstacle_found = False
+        self.step_length = 5
+
+    def avoid_obstacles(self):
+        if(self.obstacle_found):
+            if(self.speed <= 60):
+                super().avoid_obstacles()
+            else:
+                self.direction = (self.direction + 90) % 360
+                self.obstacle_found = False
+            self.speed /= 2
+            self.step_length /= 2
+
+    # Override the + and - operations here while using those dunder methods from the superclass!
+
+    def __add__(self, value):
+        super().__add__(value)
+        self.step_length += value / 2
+
+    def __sub__(self, value):
+        super().__sub__(value)
+        self.step_length -= value / 2
+
+
+robot_1 = DriveBot()
+robot_2 = WalkBot()
+
+# Uncomment these lines when you are ready to test your code!
+robot_1 + 20
+robot_1 - 10
+robot_2 + 10
+robot_2 - 5
+
+print(robot_1.speed)
+print(robot_1.sensor_range)
+print(robot_2.speed)
+print(robot_2.step_length)
