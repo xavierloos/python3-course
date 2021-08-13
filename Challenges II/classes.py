10ace3724c35b523226ab8476e8754344d158010
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:18:38 +0100

    Challenge # 5. Identifying Robots

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -127,3 +127,43 @@ DriveBot.all_disabled = True
 print(robot_1.latitude)
 print(robot_2.longitude)
 print(robot_3.all_disabled)
+
+# 5. Identifying Robots
+# In order to keep track of the robots we are creating, we want to be able to assign an ID value to each robot when it is created. If we create five robots in a row, we want the IDs of each robot to be 1, 2, 3, 4, and 5 respectively. This can be accomplished by using a class variable counter which increments and is assigned to an instance variable for the ID whenever the constructor is called. Here are the steps:
+# Create a new class variable in the DriveBot class called robot_count
+# In the constructor, increment the robot_count by 1
+# After incrementing the value, assign the value of robot_count to a new instance variable called id.
+
+
+class DriveBot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        self.motor_speed = motor_speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        DriveBot.robot_count += 1
+        self.id = DriveBot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.motor_speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+
+robot_1 = DriveBot()
+robot_1.motor_speed = 5
+robot_1.direction = 90
+robot_1.sensor_range = 10
+
+robot_2 = DriveBot(35, 75, 25)
+robot_3 = DriveBot(20, 60, 10)
+
+print(robot_1.id)
+print(robot_2.id)
+print(robot_3.id)
