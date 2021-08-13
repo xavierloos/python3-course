c6798b97fff5bd0af899de2093c9591552f21b29
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:28:18 +0100

    Challenge (Advanced) # 5. Prevent A Robot Takeover

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -489,3 +489,106 @@ print(robot_1.speed)
 print(robot_1.sensor_range)
 print(robot_2.speed)
 print(robot_2.step_length)
+
+# 5. Prevent A Robot Takeover
+# Finally, we are going to make some last additions to our bipedal robot and test it out. Your customers are worried that your new robot model might become sentient when controlled in large numbers. To prevent this, program the bipedal robots so that if five WalkBots are created when there are already at least 5 DriveBots (causing 10 or more total bots) then disable all robots. This is what you need to do:
+# Create a new class variable in the WalkBot class to count the number of WalkBot instances
+# In the constructor for the WalkBot, increment the counter by 1
+# Also, in the constructor, check if the number of total Robots (from the superclass) is greater than or equal to 10 and if the number of WalkBots is greater than or equal to 5.
+# If so, disable all robots
+
+
+class Robot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, speed=0, direction=180, sensor_range=10):
+        self.speed = speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        self.obstacle_found = False
+        Robot.robot_count += 1
+        self.id = Robot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+    def avoid_obstacles(self):
+        if self.obstacle_found:
+            self.direction = (self.direction + 180) % 360
+            self.obstacle_found = False
+
+    def __add__(self, value):
+        self.speed += value
+
+    def __sub__(self, value):
+        self.speed -= value
+
+
+class DriveBot(Robot):
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        super().__init__(motor_speed, direction, sensor_range)
+
+    def __add__(self, value):
+        super().__add__(value)
+        self.sensor_range += value
+
+    def __sub__(self, value):
+        super().__sub__(value)
+        self.sensor_range -= value
+
+
+class WalkBot(Robot):
+    walk_bot_count = 0
+
+    def __init__(self, steps_per_minute=0, direction=180, sensor_range=10, step_length=5):
+        super().__init__(steps_per_minute, direction, sensor_range)
+        self.step_length = step_length
+
+        WalkBot.walk_bot_count += 1
+        if(WalkBot.walk_bot_count >= 5 and Robot.robot_count >= 10):
+            Robot.all_disabled = True
+
+    def adjust_sensor(self, new_sensor_range):
+        super().adjust_sensor(new_sensor_range)
+        self.obstacle_found = False
+        self.step_length = 5
+
+    def avoid_obstacles(self):
+        if(self.obstacle_found):
+            if(self.speed <= 60):
+                super().avoid_obstacles()
+            else:
+                self.direction = (self.direction + 90) % 360
+                self.obstacle_found = False
+            self.speed /= 2
+            self.step_length /= 2
+
+    def __add__(self, value):
+        super().__add__(value)
+        self.step_length += value / 2
+
+    def __sub__(self, value):
+        super().__sub__(value)
+        self.step_length -= value / 2
+
+
+robot_1 = DriveBot()
+robot_2 = WalkBot()
+robot_3 = DriveBot()
+robot_4 = DriveBot()
+robot_5 = WalkBot()
+robot_6 = DriveBot()
+robot_7 = DriveBot()
+robot_8 = WalkBot()
+robot_9 = WalkBot()
+print(Robot.all_disabled)
+robot_10 = WalkBot()
+print(Robot.all_disabled)
