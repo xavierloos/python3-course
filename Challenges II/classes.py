1b8e9231db7144eb54506e4e5bc58ff482a9548d
xavierloos <xavier.loos04@gmail.com>
2021-08-13 11:22:13 +0100

    Challenge (Advanced) # 1. Robot Inheritance

--- Challenges II/classes.py
+++ Challenges II/classes.py
@@ -167,3 +167,69 @@ robot_3 = DriveBot(20, 60, 10)
 print(robot_1.id)
 print(robot_2.id)
 print(robot_3.id)
+
+# Classes (Advanced)
+# 1. Robot Inheritance
+# The generic variables and methods have been placed into a Robot class for you. You will need to create two new classes. One for DriveBot and one for WalkBot. Both of these should inherit from the Robot class. The constructor for the DriveBot will be essentially the same as the superclass constructor, but the WalkBot constructor will include an extra parameter for an instance variable called step_length. Here is what we need to do:
+# Create a new class called DriveBot which inherits from the Robot class
+# Call the superclass constructor within the DriveBot constructor but pass motor_speed as the parameter for speed in the super class
+# Create a new class called WalkBot which inherits from the Robot class
+# Add another parameter to the WalkBot constructor for step_length which defaults to 5
+# Call the superclass constructor within the constructor for the WalkBot class
+# Assign the input parameter for step_length to a new instance variable for step_length
+
+
+class Robot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
+
+    def __init__(self, speed=0, direction=180, sensor_range=10):
+        self.speed = speed
+        self.direction = direction
+        self.sensor_range = sensor_range
+        self.obstacle_found = False
+        Robot.robot_count += 1
+        self.id = Robot.robot_count
+
+    def control_bot(self, new_speed, new_direction):
+        self.speed = new_speed
+        self.direction = new_direction
+
+    def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
+    def avoid_obstacles(self):
+        if self.obstacle_found:
+            self.direction = (self.direction + 180) % 360
+            self.obstacle_found = False
+
+# Create the DriveBot class here!
+
+
+class DriveBot(Robot):
+
+    def __init__(self, motor_speed=0, direction=180, sensor_range=10):
+        super().__init__(motor_speed, direction, sensor_range)
+
+# Create the WalkBot class here!
+
+
+class WalkBot(Robot):
+
+    def __init__(self, steps_per_minute=0, direction=180, sensor_range=10, step_length=5):
+        super().__init__(steps_per_minute, direction, sensor_range)
+        self.step_length = step_length
+
+
+# Uncomment the robot instantiation!
+robot_1 = DriveBot()
+robot_2 = WalkBot()
+robot_3 = WalkBot(20, 90, 15, 10)
+
+# Use these print statements to test your code!
+
+print(robot_2.id)
+print(robot_3.step_length)
+print(robot_1.speed)
