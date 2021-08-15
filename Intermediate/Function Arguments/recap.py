edbc0340c7cc305f754ef6d08db6c536ffaad9e4
xavierloos <xavier.loos04@gmail.com>
2021-08-15 21:13:52 +0100

    Update

--- Intermediate/Function Arguments/recap.py
+++ Intermediate/Function Arguments/recap.py
@@ -46,7 +46,6 @@ print(tables)
 def assign_table(table_number, name, vip_status=False):
     tables[table_number] = [name, vip_status]
 
-
 assign_table(6, 'Yoni', False)
 print(tables)
 assign_table(table_number=3, name='Martha', vip_status=True)
