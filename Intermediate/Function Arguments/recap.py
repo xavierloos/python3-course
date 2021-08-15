e04764baff52015ce598f4c3e0d84f4f7968753f
xavierloos <xavier.loos04@gmail.com>
2021-08-15 21:14:27 +0100

    Update

--- Intermediate/Function Arguments/recap.py
+++ Intermediate/Function Arguments/recap.py
@@ -41,8 +41,12 @@ tables = {
 }
 print(tables)
 # Write your code below:
+
+
 def assign_table(table_number, name, vip_status=False):
     tables[table_number] = [name, vip_status]
+
+
 assign_table(6, 'Yoni', False)
 print(tables)
 assign_table(table_number=3, name='Martha', vip_status=True)
