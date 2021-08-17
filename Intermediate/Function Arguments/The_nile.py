08462d2ddbd4058787202c409df950f64669af35
xavierloos <xavier.loos04@gmail.com>
2021-08-17 13:07:23 +0100

    Step 1

--- Intermediate/Function Arguments/The_nile.py
+++ Intermediate/Function Arguments/The_nile.py
@@ -18,3 +18,26 @@
 # 7.What about our shoppers who hastily purchase goods without indicating their shipping type? Let’s give our function a default argument for shipping_type. Since they’re in such a hurry let’s make the default argument 'Overnight'. They’ll be happier to get what they ordered earlier, and we’ll be happier because they paid more money for it. It’s a win-win!
 
 # 8.Want to make sure you wrote the function correctly? Try calling test_function(calculate_shipping_cost) after your function definition.
+from nile import get_distance, format_price, SHIPPING_PRICES
+from test import test_function
+
+# Define calculate_shipping_cost() here:
+
+
+def calculate_shipping_cost(from_coords, to_coords, shipping_type):
+    pass
+
+# Test the function by calling
+# test_function(calculate_shipping_cost)
+
+# Define calculate_driver_cost() here
+
+
+# Test the function by calling
+# test_function(calculate_driver_cost)
+
+# Define calculate_money_made() here
+
+
+# Test the function by calling
+# test_function(calculate_money_made)
