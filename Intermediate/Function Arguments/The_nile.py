8221b462c41b4d87695e30adb6a4f40643f865df
xavierloos <xavier.loos04@gmail.com>
2021-08-17 13:09:14 +0100

    Step 6

--- Intermediate/Function Arguments/The_nile.py
+++ Intermediate/Function Arguments/The_nile.py
@@ -30,6 +30,7 @@ def calculate_shipping_cost(from_coords, to_coords, shipping_type):
     distance = get_distance(*from_coords, *to_coods)
     shipping_rate = SHIPPING_PRICES[shipping_type]
     price = distance * shipping_rate
+    return format_price(price)
 # Test the function by calling
 # test_function(calculate_shipping_cost)
 
