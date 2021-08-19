c4b65b233a4fb851e1d7a27abd510ac6f547df75
xavierloos <xavier.loos04@gmail.com>
2021-08-19 12:25:22 +0100

    Built-in Namespace

---
+++ Intermediate/Namespaces & Scope/built_in_namespace.py
@@ -0,0 +1,5 @@
+# 1.Let’s re-examine everything that exists in the built-in namespace. Use the print() function combined with the dir() function to print out __builtins__.
+# Take some time to examine the contents of the namespace further and even read about the module in the Python documentation.
+
+# Write your code below:
+print(dir(__builtins__))
