9a211fcc51186183a27d75fefc3a211cd32919d8
xavierloos <xavier.loos04@gmail.com>
2021-08-19 14:17:38 +0100

    p

---
+++ Intermediate/Namespaces & Scope/global_scope.py
@@ -0,0 +1,26 @@
+# 1.Take a look at the two functions defined. One function named print_avaliable() prints the number of gallons we have available for a specific color. The other function named print_all_colors_avaliable() simply prints all available colors!
+# Ponder what might happen when we run the script and then run it to find out!
+
+# 2.Whoops! Looks like we have an error with accessing paint_gallons_avaliable in our print_all_colors_avaliable() function. This is because the dictionary is locally scoped.
+# Fix the issue by moving paint_gallons_avaliable into the global scope.
+
+paint_gallons_avaliable = {
+    'red': 50,
+    'blue': 72,
+    'green': 99,
+    'yellow': 33
+}
+
+
+def print_avaliable(color):
+    print('There are ' +
+          str(paint_gallons_avaliable[color]) + ' gallons avaliable of ' + color + ' paint.')
+
+
+def print_all_colors_avaliable():
+    for color in paint_gallons_avaliable:
+        print(color)
+
+
+print_avaliable('red')
+print_all_colors_avaliable()
