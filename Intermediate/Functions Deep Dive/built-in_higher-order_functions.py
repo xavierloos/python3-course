f5e19e327371f0070f95008ffe3025a7dce6781e
xavierloos <xavier.loos04@gmail.com>
2021-08-20 11:52:36 +0100

    Update

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -44,7 +44,6 @@ letters = ['r', 'e', 'd', 'u', 'c', 'e']
 
 # your code below:
 # remember to import the reduce function
-
 # store the result of your reduce function in the variable word
 
 word = reduce(lambda x, y: x+y, letters)
