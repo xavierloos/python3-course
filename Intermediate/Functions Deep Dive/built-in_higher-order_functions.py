7d6e7394964a5e90991ce2ee8ab51b1069220927
xavierloos <xavier.loos04@gmail.com>
2021-08-20 11:53:15 +0100

    Space

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -43,6 +43,7 @@ print(string_titles_list)
 letters = ['r', 'e', 'd', 'u', 'c', 'e']
 
 # your code below:
+
 # remember to import the reduce function
 # store the result of your reduce function in the variable word
 
