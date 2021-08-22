c87f38e802df7be085d3239a8610dba2bdfa1500
xavierloos <xavier.loos04@gmail.com>
2021-08-22 19:41:33 +0100

    Update

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -6,6 +6,7 @@ grade_list = [3.5, 3.7, 2.6, 95, 87]
 
 # Your code below:
 # assign the result of your map function to the variable grades_100scale
+
 grades_100scale = map(lambda grade: grade*25 if grade <=
                       4.0 else grade, grade_list)
 # convert grades_100scale to a list and save it as updated_grade_list
