f781b5931af2d18119a91173cdf09249e162c38b
xavierloos <xavier.loos04@gmail.com>
2021-08-22 19:47:17 +0100

    Update

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -9,6 +9,7 @@ grade_list = [3.5, 3.7, 2.6, 95, 87]
 
 grades_100scale = map(lambda grade: grade*25 if grade <=
                       4.0 else grade, grade_list)
+                      
 # convert grades_100scale to a list and save it as updated_grade_list
 updated_grade_list = list(grades_100scale)
 # print updated_grade_list
