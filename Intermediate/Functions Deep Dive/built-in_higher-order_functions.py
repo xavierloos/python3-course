7bfc8d1d0eaa47f59ec90cb1a3832538566d4b38
xavierloos <xavier.loos04@gmail.com>
2021-08-22 19:41:16 +0100

    Update

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -3,6 +3,7 @@
 from functools import reduce
 
 grade_list = [3.5, 3.7, 2.6, 95, 87]
+
 # Your code below:
 # assign the result of your map function to the variable grades_100scale
 grades_100scale = map(lambda grade: grade*25 if grade <=
