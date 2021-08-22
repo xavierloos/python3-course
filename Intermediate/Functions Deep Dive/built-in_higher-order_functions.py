259bea1218217f9a55c94d2452426e05907d32a1
xavierloos <xavier.loos04@gmail.com>
2021-08-22 19:48:04 +0100

    Update

--- Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
+++ Intermediate/Functions Deep Dive/built-in_higher-order_functions.py
@@ -15,6 +15,7 @@ updated_grade_list = list(grades_100scale)
 
 # print updated_grade_list
 print(updated_grade_list)
+
 # We were given a list of lists, where each sublist holds the title of a famous book that has a year as its title and the last name of the author that wrote the book. Unfortunately, when this list was made, each of the books was accidentally entered twice—once with the title as a numeric value and once with the title as a string. Use the filter() function to deduplicate the list and keep only the sublists that have the book title stored as a string:
 
 books = [["Burgess", 1985], ["Orwell", "Nineteen Eighty-four"], ["Murakami", "1Q85"],
